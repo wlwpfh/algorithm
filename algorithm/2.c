2955ab01116f0b9498fab122adc779e925224fe7
godwpfh <0112jyoung@naver.com>
2021-01-14 21:04:24 +0900

    자연수의 합

---
+++ algorithm/2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+
+	int a, b;
+
+
+	scanf("%d %d", &a, &b);
+
+
+	int sum = 0;
+	for (int i = a; i < b; i++) {
+		printf("%d + ",i);
+		sum = sum + i;
+	}
+	sum = sum + b;
+	printf("%d = %d \n", b, sum);
+
+	return 0;
+}
