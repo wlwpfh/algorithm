93921f728612173814b1d3c3e4a01dc833c45342
godwpfh <0112jyoung@naver.com>
2021-01-30 23:52:21 +0900

    큰 숫자의 총 개수

---
+++ algorithm/12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main12() {
+	int n, sum = 0, c = 1, d = 9, res = 0;
+	scanf("%d", &n);
+
+	while (sum + d < n) {
+		res = res + (c * d);
+		sum = sum + d;
+		c++;
+		d = d * 10; //각 자리수의 숫자의 개수 
+	}
+	res = res + (n - sum) * c;
+	printf("%d \n", res);
+	return 0;
+}
