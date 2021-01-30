004c191658324d004551ee589fad2dd241e6bb72
godwpfh <0112jyoung@naver.com>
2021-01-30 23:14:55 +0900

    모두의 약수

--- algorithm/9.c
+++ algorithm/9.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
-int cnt[50000];
+int count[50001] = { 0 };
 int main9() {
 
 	int n;
 	scanf("%d", &n);
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (i % j == 0)
-				cnt[i - 1]++;
+	for (int i = 1; i <= n; i++) { //약수의 시작이 1부터이니까  
+		for (int j = i; j <= n; j = j + i) {
+			count[j]++;
 		}
-	} 
+	}
 
 	for (int i = 1; i <= n; i++)
-		printf("%d ", cnt[i - 1]);
+		printf("%d", count[i]);
 
 
 	return 0;
