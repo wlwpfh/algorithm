7b1f0a14da2d1e7aad876d492959862a5315a79b
godwpfh <0112jyoung@naver.com>
2021-02-09 22:09:43 +0900

    I S

---
+++ algorithm/38.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<vector>
+using namespace std;
+int main() {
+
+	int i, j, tmp, num, count = 0;
+	scanf("%d", &num);
+
+	vector <int> arr(num + 1);
+	vector <int> sorted(num + 1);
+	for (i = 1; i <= num; i++) {
+		scanf("%d", &arr[i]);
+	}
+	for (i = 1; i <= num; i++) {
+		tmp = arr[i];
+
+
+
+		for (j = 1; j <= i; j++) {
+			if (tmp < sorted[j]) {
+				count++; //나보다 더 큰 수가 얼마나 있는지 
+			}
+		}
+
+		for (j = tmp + 1; j <= num; j++) {
+			if (sorted[j] != 0) { //이미 차잇는 경우 
+				;
+			}
+			else { //비어있는 경우 
+				sorted[j] = i;
+			}
+		}
+		count++;
+	}
+
+	for (i = 1; i <= num; i++) {
+		printf("%d ", sorted[i]);
+	}
+
+	return 0;
+}
