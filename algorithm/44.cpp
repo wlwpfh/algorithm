0d75d17ea6d655c413affac1c487ab73148ce0bd
godwpfh <0112jyoung@naver.com>
2021-02-11 22:52:02 +0900

    44

---
+++ algorithm/44.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<vector>
+using namespace std;
+int main() {
+
+	int i, n, k, num = 0;
+
+	scanf("%d %d", &n, &k);
+
+	vector <int>arr(n + 1);
+	vector <int>exclude(n + 1);
+	int count = 1;
+	for (i = 1; i <= n; i++) {
+		arr[i] = i;
+	}
+
+	for (i = 1; i <= n - 1; i++) //한명씩 거르기
+	{
+		count++;
+
+		if (count == 3) {
+
+
+			count = 1;
+		}
+	}
+
+	printf("%d", num);
+
+	return 0;
+}
