254c2dce9c49d7e9115fa4f4e5b9c3449f9f58a2
godwpfh <0112jyoung@naver.com>
2021-02-19 23:24:05 +0900

    inversion sequence

--- algorithm/38.cpp
+++ algorithm/38.cpp
@@ -1,42 +1,25 @@
 #include<stdio.h>
 #include<vector>
+#include<algorithm>
 using namespace std;
-int main() {
-
-	int i, j, tmp, num, count = 0;
-	scanf("%d", &num);
-
-	vector <int> arr(num + 1);
-	vector <int> sorted(num + 1);
-	for (i = 1; i <= num; i++) {
-		scanf("%d", &arr[i]);
-	}  
-	//순서와 무엇이 더 큰지 확인하기
-	for (i = 1; i <= num; i++) {
-		tmp = arr[i];
-
-
-
-		for (j = 1; j <= i; j++) {
-			if (tmp < sorted[j]) {
-				count++; //나보다 더 큰 수가 얼마나 있는지 
-			}
-		}
-
-		for (j = tmp + 1; j <= num; j++) {
-			if (sorted[j] != 0) { //이미 차잇는 경우 
-				;
-			}
-			else { //비어있는 경우 
-				sorted[j] = i;
-			}
-		}
-		count++;
+int main38() {
+	freopen("input.txt", "rt", stdin);
+	int n, i, j, pos;
+	scanf("%d", &n);
+	vector<int> is(n + 1), os(n + 1);
+	for (i = 1; i <= n; i++) {
+		scanf("%d", &is[i]);
 	}
-
-	for (i = 1; i <= num; i++) {
-		printf("%d ", sorted[i]);
+	for (i = n; i >= 1; i--) {
+		pos = i;
+		for (j = 1; j <= is[i]; j++) {
+			os[pos] = os[pos + 1];
+			pos++;
+		}
+		os[pos] = i;
 	}
-
+	for (i = 1; i <= n; i++) {
+		printf("%d ", os[i]);
+	}
 	return 0;
 }
