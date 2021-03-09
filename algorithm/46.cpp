4250c7d2b2606da8efbdb645ca84a559e496b666
godwpfh <0112jyoung@naver.com>
2021-03-09 15:26:22 +0900

    먹방

--- algorithm/46.cpp
+++ algorithm/46.cpp
@@ -1,18 +1,22 @@
 #include<stdio.h>
 #include<vector>
 using namespace std;
-int main() {
-	int n, k; int i, count = 0, index = -1, curr;
-	//bool is_finished=false;
+int main46_() {
+	int n, k; int i, count = 0;
+	int sum = 0;
 	scanf("%d", &n);
 	vector<int>arr(n + 1);
 	for (i = 1; i <= n; i++) {
 		scanf("%d", &arr[i]);
+		sum = sum + arr[i];
 	}
 	scanf("%d", &k);
-
+	if (k >= sum) {
+		printf("-1");
+		return 0;
+	}
 	int j = 1;
-	while (count < k) { //일을 하기 
+	while (1) { //일을 하기 
 		if (j > n)
 		{
 			j = 1;
@@ -22,17 +26,18 @@ int main() {
 			arr[j]--;
 			count++;
 		}
+		if (count == k)
+			break;
 		j++;
 	}
+	while (1) {
+		j++;
+		if (j > n)
+			j = 1;
+		if (arr[j] != 0)
+			break;
 
-	for (i = 1; i <= n; i++) { //해당 순번 출력하기 
-		if (arr[i] != 0)
-		{
-			printf("%d", i);
-			return 0;
-		}
 	}
-
-	printf("-1");
+	printf("%d", j);
 	return 0;
 }
