c1eb54781959c5c628354c8d566c1c4cb538af80
godwpfh <0112jyoung@naver.com>
2021-04-14 23:29:38 +0900

    하고 있음..

--- algorithm/64.cpp
+++ algorithm/64.cpp
@@ -3,20 +3,20 @@
 int n, m, count = 0, graph[21][21];
 
 void path_finding(int i, int j) {
-	if (j == 5) { //끝내야함 
-		count++;
-	}
-	if (graph[i][j] == 1) {
-		path_finding(j, i);
-	}
-	else { //0인 경우 다른 길을 찾아야 함 
-		if (i != j) //자기 자신 값이 0이니까	 
-		{
+	if (i <= n && j <= n) {
 
+		if (j == n) { //끝내야함 
+			count++;
+		}
+		if (graph[i][j] == 1 && j > i) {
+			path_finding(j, 1);
+
+		}
+		else { //0인 경우 다른 길을 찾아야 함 
 			path_finding(i, j++);
+
 		}
 	}
-
 
 }
 int main() {
@@ -27,7 +27,11 @@ int main() {
 	for (i = 1; i <= m; i++) {
 		scanf("%d %d", &a, &b);
 		graph[a][b] = 1;
+
 	}
+
+
+
 
 	path_finding(1, 1);
 
