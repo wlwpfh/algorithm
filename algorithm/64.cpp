bae6af06ade7890b8e240e5692dedc4172018554
godwpfh <0112jyoung@naver.com>
2021-04-17 21:09:17 +0900

    경로 탐색

--- algorithm/64.cpp
+++ algorithm/64.cpp
@@ -1,32 +1,42 @@
-#include<stdio.h>	
-int map[30][30], ch[30], cnt = 0, n;
-void DFS(int v) {
+#include<stdio.h>
+
+int n, m, count = 0, graph[21][21], path[21];
+
+void path_finding(int dest) { //도착 지점 
 	int i;
-	if (v == n) {
-		cnt++;
+	if (dest == n) { //끝내야함 
+		count++;
 	}
 	else {
-		for (i = 1; i <= n; i++) {
-			if (map[v][i] == 1 && ch[i] == 0) {
-				ch[i] = 1;
-				DFS(i);
-				ch[i] = 0;
+		for (i = 1; i <= m; i++) {
+			if (graph[dest][i] == 1 && path[i] == 0) {
+				path[i] = 1;
+				path_finding(i);
+				path[i] = 0;
 			}
 		}
 	}
-}
 
+
+
+}
 int main() {
-	freopen("input.txt", "rt", stdin);
-	int m, i, j, a, b, c;
+	int i, j, a, b;
+
 	scanf("%d %d", &n, &m);
+
 	for (i = 1; i <= m; i++) {
 		scanf("%d %d", &a, &b);
-		map[a][b] = 1;
+		graph[a][b] = 1;
+
 	}
-	ch[1] = 1;
-	DFS(1);
-	printf("%d\n", cnt);
+
+	path[1] = 1;
+
+
+	path_finding(1);
+
+	printf("%d", count);
+
 	return 0;
 }
-
