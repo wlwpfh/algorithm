98170eaa003b311e4f1ab427b1c84a67bcf5b8b9
godwpfh <0112jyoung@naver.com>
2021-04-21 23:29:23 +0900

    하는 중

--- algorithm/66.cpp
+++ algorithm/66.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 int n, m, count;
-vector<int>adlist(30);
+
+vector<pair<int, int> > adlist[30];
 
 void path_finding(int num) {
 	int i;
@@ -12,24 +13,21 @@ void path_finding(int num) {
 		count++;
 	}
 	else {
-		for (i = 1; i <= n; i++) {
-			printf("%d ", adlist[i]);
+	}
+
+	int main() {
+
+		int i, a, b;
+
+		scanf("%d %d", &n, &m);
+
+		for (i = 1; i <= m; i++) {
+			scanf("%d %d", &a, &b);
+			adlist[i].push_back({ a,b });
 		}
+
+		path_finding(1);
+
+		printf("%d \n", count);
+		return 0;
 	}
-}
-
-int main() {
-	int i, a, b;
-	scanf("%d %d", &n, &m);
-
-	for (i = 1; i <= m; i++) {
-		scanf("%d %d", &a, &b);
-		adlist[a] = b;
-		adlist[b] = a;
-	}
-
-	path_finding(1);
-
-	printf("%d \n", count);
-	return 0;
-}
