a180f75f51e047b2381f88a1f1e92c73b4aa7da9
godwpfh <0112jyoung@naver.com>
2021-04-22 10:26:13 +0900

    경로 탐색 - 인접리스트 활용

--- algorithm/66.cpp
+++ algorithm/66.cpp
@@ -4,30 +4,38 @@
 using namespace std;
 
 int n, m, count;
-
-vector<pair<int, int> > adlist[30];
+int check[30];
+vector<int> adlist[30];
 
 void path_finding(int num) {
 	int i;
 	if (num == n) {
 		count++;
+
 	}
 	else {
-	}
-
-	int main() {
-
-		int i, a, b;
-
-		scanf("%d %d", &n, &m);
-
-		for (i = 1; i <= m; i++) {
-			scanf("%d %d", &a, &b);
-			adlist[i].push_back({ a,b });
+		for (i = 0; i < adlist[num].size(); i++) {
+			if (check[adlist[num][i]] == 0) {
+				check[adlist[num][i]] = 1;
+				path_finding(adlist[num][i]);
+				check[adlist[num][i]] = 0;
+			}
 		}
-
-		path_finding(1);
-
-		printf("%d \n", count);
-		return 0;
 	}
+}
+int main() {
+
+	int i, a, b;
+
+	scanf("%d %d", &n, &m);
+
+	for (i = 1; i <= m; i++) {
+		scanf("%d %d", &a, &b);
+		adlist[a].push_back(b);
+	}
+	check[1] = 1;
+	path_finding(1);
+
+	printf("%d \n", count);
+	return 0;
+}
