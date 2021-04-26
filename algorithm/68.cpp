06c31e9e9d4f6e05231043aba8aa9ca81c28714d
godwpfh <0112jyoung@naver.com>
2021-04-26 23:34:49 +0900

    하는 중

---
+++ algorithm/68.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<vector>
+
+using namespace std;
+
+vector<int>adlist[21];
+int n, m, cost = 10000;
+int check[30];
+
+int main() {
+	int i, a, b, c;
+	scanf("%d %d", &n, &m);
+
+	for (i = 1; i <= m; i++) {
+		scanf("%d %d %d", &a, &b, &c);
+
+	}
+
+}
