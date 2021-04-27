5f752189c76c7323f98bbc5b86b64d6f357116d7
godwpfh <0112jyoung@naver.com>
2021-04-27 21:49:37 +0900

    최소비용 - 방향 그래프 인접리스트 (pair사용)

--- algorithm/68.cpp
+++ algorithm/68.cpp
@@ -3,17 +3,38 @@
 
 using namespace std;
 
-vector<int>adlist[21];
+vector<pair<int, int> >adlist[21];
 int n, m, cost = 10000;
-int check[30];
-
+int check[21];
+void least_cost(int num, int sum) {
+	int i;
+	if (num == n) {
+		if (cost > sum)
+			cost = sum;
+	}
+	else {
+		for (i = 0; i < adlist[num].size(); i++) {
+			if (check[adlist[num][i].first] == 0) {
+				check[adlist[num][i].first] = 1;
+				least_cost(adlist[num][i].first, sum + adlist[num][i].second);
+				check[adlist[num][i].first] = 0;
+			}
+		}
+	}
+}
 int main() {
 	int i, a, b, c;
 	scanf("%d %d", &n, &m);
 
 	for (i = 1; i <= m; i++) {
 		scanf("%d %d %d", &a, &b, &c);
-
+		adlist[a].push_back(make_pair(b, c));
 	}
+	check[1] = 1;
+	least_cost(1, 0);
+
+	printf("%d", cost);
+
+	return 0;
 
 }
