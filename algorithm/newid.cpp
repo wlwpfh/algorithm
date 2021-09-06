ba2493a7acb3329ee97b13fdeda3847c6506521c
godwpfh <0112jyoung@naver.com>
2021-09-06 23:27:52 +0900

    level 1 - 새로운 아이디 △

--- algorithm/newid.cpp
+++ algorithm/newid.cpp
@@ -1,5 +1,5 @@
 #include <string>
-#include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -7,38 +7,53 @@ string solution(string new_id) {
     int i, end_count = 0;
     string answer = "";
     for (i = 0; i < new_id.length(); i++) {
-        if (new_id[i] >= 65 && new_id[i] <= 90)
+        if (new_id[i] >= 65 && new_id[i] <= 90) //1
             new_id[i] += 32;
     }
-
+    cout << new_id << endl;
     for (i = 0; i < new_id.length(); i++) {
-        if ((new_id[i] >= 65 && new_id[i] <= 90) || (new_id[i] > 0 && new_id[i] < 9)
+        if ((new_id[i] >= 65 && new_id[i] <= 90) || (new_id[i] >= 0 && new_id[i] <= 9)
             || new_id[i] == 45 || new_id[i] == 95 || new_id[i] == 44) {
             //answer[i].append(new_id[i]);
         }
         else {
-            new_id.erase(i);
-//      answer[i].append(new_id[i]);
+            new_id.erase(remove(new_id.begin(), new_id.end(), new_id[i]),
+                new_id.end());
+            //answer[i].append(new_id[i]);
         }
     }
+    cout << new_id << endl;
 
 
 
-    for (i = 1; i < new_id.length(); i++) {
+    for (i = 1; i < new_id.length(); i++) { //3
         if (new_id[i] == 44 && new_id[i - 1] == 44) {
-            new_id[i] = 32;
+            new_id.erase(i);
         }
     }
 
-    if (new_id[0] == 44) {
-        new_id[0] = 32;
+    if (new_id[0] == 44) { //4
+        new_id.erase(0);
     }
-    // if(new_id[new_id.length()-1]==44){
-    //     new_id[new_id.length()-1]=32;
-    // }
+    else if (new_id[new_id.length() - 1] == 44) {
+        new_id.erase(new_id.length() - 1);
+    }
+
+    cout << new_id << endl;
+
+
 
 
 
 
     return answer;
+}
+
+template <class ForwardIt, class UnaryPredicate>
+ForwardIt remove_if(ForwardIt first, ForwardIt last, UnaryPredicate p) {
+    first = std::find_if(first, last, p);
+    if (first != last)
+        for (ForwardIt i = first; ++i != last;)
+            if (!p(*i)) *first++ = std::move(*i);
+    return first;
 }
