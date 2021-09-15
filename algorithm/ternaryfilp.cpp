e94ec711c891e1b66e1e8f78e7e31ba0f12592a0
godwpfh <0112jyoung@naver.com>
2021-09-15 22:40:30 +0900

    90점

--- algorithm/ternaryfilp.cpp
+++ algorithm/ternaryfilp.cpp
@@ -1,37 +1,43 @@
 #include <string>
 #include <vector>
 #include <cmath>
+
 using namespace std;
 
 int solution(int n) {
-    int rest = 0, three_num = 0, tmp, new_count = 0, new_three = 0, count = 0, answer = 0;
+    int rest = 0, count = 0, new_count = 0;
+    long long three_num = 0, tmp, new_three = 0, answer = 0;
 
     while (1) {
         if (n < 3) {
-            three_num = pow(10, count) * n + three_num;
+            three_num += pow(10, count) * n;
             count++;
+            printf("3진법으로 %d \n", three_num);
             break;
         }
         else {
             rest = n % 3;
             n = n / 3;
-            three_num = pow(10, count) * rest + three_num;
+            three_num += pow(10, count) * rest;
             count++;
-
+            printf("3진법으로 %d \n", three_num);
         }
+
     }
-    printf("%d %d", three_num, count);
+    printf("%d %d \n", three_num, count);
 
     while (1) {
         if (new_count == count) {
-            printf("%d", new_three);
+            answer += three_num * 1;
             break;
         }
         else {
             tmp = three_num % 10;
             three_num = three_num / 10;
-            new_three = pow(10, count - new_count) * tmp + new_three;
             new_count++;
+            answer += pow(3, count - new_count) * tmp;
+            printf("몫:%d 나머지:%d answer: %d count:%d \n", three_num, tmp, answer, new_count);
+
         }
     }
 
