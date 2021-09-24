10ec5a5816b89e9cbae6d4c097df673f8d3e546d
godwpfh <0112jyoung@naver.com>
2021-09-24 22:04:17 +0900

    하는 중

---
+++ algorithm/bringmiddleletter.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+
+    if (s.length() % 2 == 0) {
+        //printf("%c %c",s[s.length()/2],s[s.length()/2-1]);
+        answer = s[s.length() / 2 - 1];
+        answer += s[s.length() / 2];
+    }
+    else {
+        answer = s[s.length() / 2];
+    }
+    return answer;
+}
--- algorithm/numstringeng.cpp
+++ algorithm/numstringeng.cpp
@@ -15,23 +15,28 @@ int solution(string s) {
         if (s[i] >= 48 && s[i] <= 57) {
             collect[i] = s[i] - 48;
             printf("숫자는 %d \n", collect[i]);
-            printf("현재 s는 %s \n", s.c_str());
+            s.erase(i, 1);
+            printf("현재 남은 s는 %s \n", s.c_str());
         }
         else {
             for (j = 0; j < word.size(); j++) {
-                if (s.find(word[j]) != -1) {
+                if (s.find(word[j]) != -1) { //찾았다면 없애고 탈출하기
                     printf("문자인 %d %d %d \n", j, s.find(word[j]), word[j].length());
                     collect[i] = j;
-                    s.erase(s.find(word[j]), s.find(word[j]) + word[j].length());
-                    printf("현재 s는 %s \n", s.c_str());
+                    s.erase(s.find(word[j]), word[j].length());
+                    printf("현재 남은 s는 %s \n", s.c_str());
                     break;
+
+
                 }
             }
         }
 
     }
 
-
+    for (i = 0; i < collect.size(); i++) {
+        printf("%d ", collect[i]);
+    }
 
     return answer;
 }
