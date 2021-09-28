07017ebe082c1ead0da505b04d4310ed0ccdb000
godwpfh <0112jyoung@naver.com>
2021-09-28 14:20:22 +0900

    level 1 - 평균 구하기

---
+++ algorithm/makeavg.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    int i, sum = 0;
+    for (i = 0; i < arr.size(); i++) {
+        sum += arr[i];
+    }
+    answer = (double)sum / arr.size();
+    return answer;
+}
