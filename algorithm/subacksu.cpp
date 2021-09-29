ea1449d8cdfc7010189ba4829dd0ba856304a6da
godwpfh <0112jyoung@naver.com>
2021-09-29 16:01:44 +0900

    level 1 - 수박수

---
+++ algorithm/subacksu.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    int i;
+    for (i = 0; i < n / 2; i++) {
+        answer += "¼ö¹Ú";
+    }
+    if (n % 2 != 0) {
+        answer += "¼ö";
+    }
+    return answer;
+}
