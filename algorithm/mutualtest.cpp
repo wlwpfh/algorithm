e56b9ba92c71e4901e2a7e21fbc714e424cc2772
godwpfh <0112jyoung@naver.com>
2021-10-13 22:49:13 +0900

    level 1 - 2주차_상호평가

--- algorithm/mutualtest.cpp
+++ algorithm/mutualtest.cpp
@@ -5,30 +5,34 @@ using namespace std;
 
 string solution(vector<vector<int>> scores) {
     string answer = "";
-    int i, j, min = 0, max = 0, sum = 0;
+    //vector<int> same(scores.size());
+    int i, j, min = 100, max = 0, same_min, same_max, sum = 0;
 
     vector<double>avg(scores.size());
     for (i = 0; i < scores.size(); i++) {
-        min = scores[i][i];
-        max = min;
+        same_min = 0, same_max = 0;
+        min = 1000;
+        max = 0;
         for (j = 0; j < scores[i].size(); j++) {
+            if (min == scores[j][i])
+                same_min++;
+            if (max == scores[j][i])
+                same_max++;
 
             if (min > scores[j][i])
                 min = scores[j][i];
             if (max < scores[j][i])
                 max = scores[j][i];
             sum += scores[j][i];
-            printf("%d번째 학생의 점수에 %d를 더해 총 %d점이다. \n", i, scores[j][i], sum);
-
 
         }
-        if (min == scores[i][i] || max == scores[i][i]) {
+        if ((min == scores[i][i] && same_min == 0) || (max == scores[i][i] && same_max == 0)) {
             avg[i] = (double)(sum - scores[i][i]) / (scores.size() - 1);
-            printf("%d째 학생은 최솟값, 최댓값을 가져 평균이 %f \n", i, avg[i]);
+
         }
         else {
             avg[i] = (double)sum / (scores.size());
-            printf("%d째 학생은 평균이 %f \n", i, avg[i]);
+
         }
         sum = 0;
     }
