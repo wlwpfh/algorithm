20f15083d075d1160b1e01cc6015589ed2139d03
godwpfh <0112jyoung@naver.com>
2021-11-11 14:31:10 +0900

    하는 중

---
+++ algorithm/developfunc.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    queue<int> q;
+    int i, tmp, a, count = 0;
+
+    for (i = 0; i < progresses.size(); i++) {
+        tmp = 100 - progresses[i];
+        if (tmp % speeds[i] == 0)
+            q.push(tmp / speeds[i]);
+        else
+            q.push(tmp / speeds[i] + 1);
+    }
+    while (!q.empty()) {
+        count = 0;
+        tmp = q.front();
+        q.pop();
+        count++;
+        if (tmp >= q.front()) { //앞에 나보다 더 값이 작은 애들이 있으면 그걸 세서 넣기
+            count++;
+            printf("%d가 %d보다 작기 때문에 같이 나온다. \n", q.front(), tmp);
+            q.pop();
+
+
+        }
+        answer.push_back(count);
+    }
+    return answer;
+}
