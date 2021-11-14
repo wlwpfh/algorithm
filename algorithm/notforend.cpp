ad87951bf0b99f13625c23940ad461aea1567ec2
godwpfh <0112jyoung@naver.com>
2021-11-15 00:24:10 +0900

    중복된 값 찾기 하는 중

--- algorithm/notforend.cpp
+++ algorithm/notforend.cpp
@@ -2,21 +2,40 @@
 #include <vector>
 #include <unordered_map>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
-    int i, j;
+    int i, j, count = 1;
     unordered_map< string, int> checked;
+    unordered_map< string, int> forPrint;
+
     for (i = 0; i < participant.size(); i++) {
-        checked.insert(make_pair(participant[i], 0));
+        //if(checked.find(participant[i]))
+        {checked.insert(make_pair(participant[i], 0));
+        printf("bool: %d particpant: %s \n", checked.find(participant[i]), participant[i].c_str()); }
     }
 
     for (i = 0; i < completion.size(); i++) {
         if (checked.count(completion[i]))
             checked[completion[i]] = 1;
     }
-    for (auto& x : checked)
-        if (x.second == 0)
-            answer = x.first;
+    for (pair<string, int> k : checked)
+    {
+
+        if (k.second != 1)
+            answer = k.first;
+    }
+    if (answer == "") {
+        // for(pair<string, int> k : checked){
+        //     pair<string, int> count=find(participant.begin(), participant.end(), k.first);
+        //     printf("%s %d",count.first, count.second);
+
+        //}
+        printf("%d", equal(participant.begin(), participant.end(), completion.begin()));
+        answer = participant[equal(participant.begin(), participant.end(), completion.begin())];
+    }
+
+
     return answer;
 }
