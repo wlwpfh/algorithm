3b0762a6ca0d6da647b339a80b35eb010a5a505d
godwpfh <0112jyoung@naver.com>
2021-11-19 16:41:30 +0900

    하는 중

--- algorithm/72.cpp
+++ algorithm/72.cpp
@@ -9,24 +9,36 @@ int main() {
 	scanf("%d %d", &n, &k);
 	vector<int>check(n + 1);
 
-	int num, curr = k;
+	int count = 0, past, num, curr = k;
 	check[k] = 1;
+
 	Q.push(k);
 	while (1) {
-		printf("현재 당번: %d, 미래 예상 당번: %d \n", curr, curr + k);
+		count = 0;
 		if (Q.size() == n - 1)
 			break;
 		if (k + curr > n) {
-			curr = (k + curr) % k;
+			past = curr;
+			curr = (k + curr) % n;
+			printf("curr: %d \n", curr);
 		}
 		else {
+			past = curr;
 			curr = curr + k;
 		}
+		//	printf("현재 당번: %d \n",curr);
+		for (i = past + 1; i < curr; i++) {
+			if (check[i] == 1) {
+				count++;
+			}
 
-		if (check[curr] == 0) {
+		}
+		printf("%d와 %d사이에 %d개의 체크된 값이 있다 \n", past + 1, curr, count);
+		if (check[curr + count] == 0) {
 			check[curr] = 1;
 			Q.push(curr);
-			printf("넣은 것: %d \n", curr);
+			printf("들어간 값: %d \n", curr);
+
 		}
 
 	}
