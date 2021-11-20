0c4609e2a51451007e638b4a0e1b876de1b9ddd9
godwpfh <0112jyoung@naver.com>
2021-11-20 21:07:28 +0900

    level 1 - 완주하지 못한 선수 (hash - unordered_map 이용)

--- algorithm/notforend.cpp
+++ algorithm/notforend.cpp
@@ -1,41 +1,22 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <iostream>
-#include <algorithm>
 using namespace std;
 string solution(vector<string> participant, vector<string> completion) {
-    string answer = "";
-    int i, j, count = 1;
+    string answer;
     unordered_map< string, int> checked;
-    unordered_map< string, int> forPrint;
 
-    for (i = 0; i < participant.size(); i++) {
-        //if(checked.find(participant[i]))
-        {checked.insert(make_pair(participant[i], 0));
-        printf("bool: %d particpant: %s \n", checked.find(participant[i]), participant[i].c_str()); }
-    }
+    for (string name : participant)
+        checked[name]++;
 
-    for (i = 0; i < completion.size(); i++) {
-        if (checked.count(completion[i]))
-            checked[completion[i]] = 1;
-    }
+    for (string name : completion)
+        checked[name]--;
     for (pair<string, int> k : checked)
     {
-
-        if (k.second != 1)
-            answer = k.first;
-    }
-    if (answer == "") {
-        // for(pair<string, int> k : checked){
-        //     pair<string, int> count=find(participant.begin(), participant.end(), k.first);
-        //     printf("%s %d",count.first, count.second);
-
-        //}
-        printf("%d", equal(participant.begin(), participant.end(), completion.begin()));
-        answer = participant[equal(participant.begin(), participant.end(), completion.begin())];
+        if (k.second == 1)
+        {
+            return k.first;
+        }
     }
 
-
-    return answer;
 }
