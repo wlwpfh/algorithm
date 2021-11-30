ba16ef85c43be5b97aefeb95a96e7d4f8c6bff74
godwpfh <0112jyoung@naver.com>
2021-11-30 22:20:45 +0900

    11,14,16,17,18,20 3 하는 중

--- algorithm/listofphonenum.cpp
+++ algorithm/listofphonenum.cpp
@@ -11,64 +11,30 @@ bool solution(vector<string> phone_book) {
     string tmp;
     unordered_map<string, int> dup;
     for (string num : phone_book) {
-        dup[num.substr(0, 3)]++;
+        dup[num.substr(0, 4)]++;
     }
     printf("\n");
 
     for (pair <string, int> i : dup) {
-
+        k = 0;
         if (i.second > 1) {
             answer = false;
             break;
         }
         else {
-            //그래도 더 작은 수가 있을 수 있음
-            // for(k=0;k<phone_book.size();k++){
-            //     auto a=find(phone_book.begin(), phone_book.end(),i.first);
-            //         //자기 자신인 경우는 빼야 함
-            //     //printf("%d \n",find(phone_book.begin(), phone_book.end(),i.first));
-            //     i.second++;
 
-            //     if(find(phone_book.begin(), phone_book.end(), i.second) && j!=k){
-            //     answer=false;
-            //     break;
-            // }
-        //}
-       // j++;
             for (string num : phone_book) {
-                tmp = num.substr(0, 3);
-                printf("tmp는 %s \n", tmp.c_str());
+                if (num.substr(0, 2) == i.first && k != j) {
+                    answer = false;
+                    printf("num는 %s이고 i는 %s이다. \n", num.c_str(), i.first.c_str());
+                    break;
+                }
 
-                if (tmp)
+                k++;
             }
-            //i.first 
+
         }
+        j++;
     }
-
-    //     for(pair <string ,int > i : dup){
-    //         for(string num : phone_book){
-    //         j=0;
-    //        if(i.first==num)
-    //        {answer=false;
-    //         cout << i.first +"는" << num +"을 갖고 있음"  <<endl;
-    //        // break;
-    //        }else
-    //             answer=true;
-
-
-
-    //           // printf("j:%d , k:%d \n",j,k);
-
-
-
-    //             j++;
-
-
-    //           // printf("j:%d , k:%d \n",j,k);
-
-    //             j++;
-    //         }
-    //       k++;
-    //     }
     return answer;
 }
