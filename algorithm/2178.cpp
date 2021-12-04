0097f5152731e232537fd38a95ec0feb05653fc2
godwpfh <0112jyoung@naver.com>
2021-12-04 22:23:40 +0900

    하는 중

--- algorithm/2178.cpp
+++ algorithm/2178.cpp
@@ -20,33 +20,33 @@ int main() {
 	queue< pair<int, int> >q;
 	q.push(make_pair(0, 0));
 	int x = 0, y = 0;
-	while (1) {
-		if (x == n - 1 && y == m - 1)
-			break;
+	while (!q.empty() && !(x == n - 1 && y == m - 1)) {
+		x = q.front().first;
+		y = q.front().second;
+		q.pop();
+
 		for (i = 0; i < 4; i++) {
 			nx = x + dx[i];
 			ny = y + dy[i];
-			printf("갈 길은 [%d][%d]이다. \n", nx, ny);
 			if (nx<0 || nx>m || ny<0 || ny>n) {
-				printf("길이 범위를 초과해서 못간다. \n");
+
 				continue;
 			}
 			if (arr[nx][ny] == 0 || checked[nx][ny] == 1) { //벽이거나 이미 갔다 온  
-				printf("[%d][%d]는 벽이다 \n", nx, ny);
+
 				continue;
 			}
 			else {
 				checked[nx][ny] = 1;
 				q.push(make_pair(nx, ny));
-				x = nx;
-				y = ny;
-				printf("[%d][%d]로 이동했다 \n", x, y);
+
+				printf("[%d][%d]로 이동했다 \n", nx, ny);
 				answer++;
 				//q.pop();
-				break;
+			//	break;
 			}
 		}
 	}
-
+	printf("answer: %d \n", answer);
 	return 0;
 }
