28b4dbffa981422b7c7b8234d4eaf5fec5a2b525
godwpfh <0112jyoung@naver.com>
2021-12-05 22:05:50 +0900

    2178 - 미로탐색 (BFS)

--- algorithm/2178.cpp
+++ algorithm/2178.cpp
@@ -3,50 +3,49 @@
 using namespace std;
 
 int main() {
-
 	int n, m;
 	scanf("%d %d", &n, &m);
-	int dx[4] = { -1,1,0,0 };
-	int dy[4] = { 0,0,-1,1 };
-	int nx, ny;
+	int dx[4] = { 1,-1,0,0 };
+	int dy[4] = { 0,0,1,-1 };
 	int arr[n][m];
+	int answer[n][m] = { 0, };
 	int i, j;
-	int answer = 0;
 	int checked[n][m];
 	for (i = 0; i < n; i++)
 		for (j = 0; j < m; j++)
 			scanf("%1d", &arr[i][j]);
 
+
 	queue< pair<int, int> >q;
 	q.push(make_pair(0, 0));
+	checked[0][0] = 1;
 	int x = 0, y = 0;
-	while (!q.empty() && !(x == n - 1 && y == m - 1)) {
+	answer[0][0] = 1;
+	while (!q.empty()) {
+
 		x = q.front().first;
 		y = q.front().second;
 		q.pop();
 
 		for (i = 0; i < 4; i++) {
-			nx = x + dx[i];
-			ny = y + dy[i];
-			if (nx<0 || nx>m || ny<0 || ny>n) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
 
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
 				continue;
 			}
 			if (arr[nx][ny] == 0 || checked[nx][ny] == 1) { //벽이거나 이미 갔다 온  
-
-				continue;
+				;
 			}
 			else {
+				answer[nx][ny] = answer[x][y] + 1;
 				checked[nx][ny] = 1;
-				q.push(make_pair(nx, ny));
 
-				printf("[%d][%d]로 이동했다 \n", nx, ny);
-				answer++;
-				//q.pop();
-			//	break;
+				q.push(make_pair(nx, ny));
 			}
 		}
 	}
-	printf("answer: %d \n", answer);
+
+	printf("%d", answer[n - 1][m - 1]);
 	return 0;
 }
