8349c632602efe96dcbe5ce89ef5559a45593198
godwpfh <0112jyoung@naver.com>
2021-12-06 22:12:26 +0900

    하는 중

--- algorithm/2108.cpp
+++ algorithm/2108.cpp
@@ -3,36 +3,36 @@
 #include<algorithm>
 #include<cmath>
 using namespace std;
-
+int count[8001];
 int main() {
 	int n, freq = 0, count = 0, num = 0;
 	scanf("%d", &n);
-	int sum = 0;
+	int i, sum = 0;
 	vector<int>arr(n);
-	for (int i = 0; i < n; i++) {
+	for (i = 0; i < n; i++) {
 		scanf("%d", &arr[i]);
 		sum += arr[i];
+		if (arr[i] >= 0) {
+			count[arr[i] + 4000]++;
+		}
+		else
+			count[arr[i] - 4000]++;
 	}
 	sort(arr.begin(), arr.end());
-	for (int i = 0; i < n; i++) {
-		for (int j = i + 1; j < n; j++) {
-			if (arr[i] == arr[j]) {
-				freq++;
-			}
-			if (freq >= count) {
-				num = arr[i];
-				count = freq;
-			}
 
+	int freq = 0, num = 0;
+
+	for (i = 0; i < 8001; i++) {
+		if (count[i] > freq) {
+			freq = count[i];
+			num = i + 4000;
 		}
 	}
 
 
-
 	printf("%.0f \n", round((double)sum / (double)n));
 	printf("%d \n", arr[n / 2]);
-	if (freq == 0) printf("%d \n", arr[1]);
-	else printf("%d \n", num);
+
 	printf("%d \n", arr[n - 1] - arr[0]);
 
 	arr.clear()
