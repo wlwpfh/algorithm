76e9fcfce4a720838c4929ec39f53f76afdacfaf
godwpfh <0112jyoung@naver.com>
2021-12-06 21:22:22 +0900

    1260 - BFS와 DFS

--- algorithm/1260.cpp
+++ algorithm/1260.cpp
@@ -1,41 +1,58 @@
 #include<stdio.h>
-#include<vector>
+#include<queue>
 
 using namespace std;
 
-int front = -1, back = -1;
-vector<int> map[10000];
-int checked[10000];
-void dfs(int x) {
-    checked[x] = 1;
+int n, m;
+int arr[1001][1001];
+int checked[1001];
+queue<int> q;
 
-    printf("%d ", x);
-
-    for (int i = 0; i < map[x].size(); i++) {
-        int y = map[x][i];
-        if (checked[y] == 0)
-            dfs(y);
-    }
+void bfs(int start) {
+	checked[start] = 1;
+	q.push(start);
+	printf("%d ", start);
+	while (!q.empty()) {
+		int x = q.front();
+		q.pop();
+		for (int i = 1; i <= n; i++) {
+			if (arr[x][i] == 1 && checked[i] == 0) {
+				checked[i] = 1;
+				q.push(i);
+				printf("%d ", i);
+			}
+		}
+	}
 }
-
+void dfs(int start) {
+	checked[start] = 1;
+	printf("%d ", start);
+	for (int i = 1; i <= n; i++) {
+		if (arr[start][i] == 1 && checked[i] == 0)
+			dfs(i);
+	}
+}
 int main() {
-    int x;
-    int n, m, s;
-    int i;
-    int a = 0;
-    int b;
-    scanf("%d %d %d", &n, &m, &s);
-    vector<int> map[m];
+	int x;
+	int s;
+	int i;
+	int a = 0;
+	int b;
+	scanf("%d %d %d", &n, &m, &s);
 
-    for (i = 0; i < m; i++) {
-        scanf("%d %d", &a, &b);
-        map[a].push_back(b);
-    }
+	for (i = 0; i < m; i++) {
+		scanf("%d %d", &a, &b);
+		arr[a][b] = 1;
+		arr[b][a] = 1;
+	}
+	dfs(s);
 
-    dfs(s);
+	for (i = 1; i <= n; i++)
+		checked[i] = 0;
+	printf("\n");
+	bfs(s);
 
 
 
-
-    return 0;
+	return 0;
 }
