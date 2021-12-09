e8d85f8b288743e16397f1103ea20e3e12c3168e
wlwpfh <0112jyoung@naver.com>
2021-12-09 15:01:38 +0900

    하는 중 - 결과가 25 25로 나옴

--- algorithm/10026.cpp
+++ algorithm/10026.cpp
@@ -1,19 +1,62 @@
 #include<queue>
 #include<iostream>
+#include<cstring>
 using namespace std;
 
 int dx[4] = { -1,1,0,0 };
 int dy[4] = { 0,0,-1,1 };
 char c[101][101];
 int checked[101][101];
-int color[101][101];
-int redgreen[101][101];
+
+
+int n;
+int i;
+void bfs(int x, int y, bool redgreen) {
+	queue<pair<int, int> >q;
+	q.push(make_pair(x, y));
+	checked[x][y] = 1;
+
+	while (!q.empty()) {
+		int cx = q.front().first;
+		int cy = q.front().second;
+		q.pop();
+
+		for (i = 0; i < 4; i++) {
+			int nx = cx + dx[i];
+			int ny = cy + dy[i];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+				continue;
+			if (checked[nx][ny] == 1)
+				continue;
+			if (redgreen == true) {
+				if (c[cx][cy] == 'R' || c[cx][cy] == 'G') {
+					if (c[nx][ny] == 'G' || c[nx][ny] == 'R') {
+						q.push(make_pair(nx, ny));
+						checked[nx][ny] = 1;
+					}
+				}
+			}
+			else {
+				if (c[cx][cy] == c[nx][ny]) {
+					q.push(make_pair(nx, ny));
+					checked[nx][ny] = 1;
+
+				}
+			}
+
+		}
+	}
+
+}
+
+
 int main() {
 	int n;
 	scanf("%d", &n);
 
 	int i, j;
-
+	int a = 0, b = 0;
 	int answer = 0;
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < n; j++) {
@@ -21,70 +64,33 @@ int main() {
 
 		}
 	}
-	queue<pair<int, int> >q;
-	//	for(i=0;i<n;i++){
-	//		for(j=0;j<n;j++){
-	//			printf("%c ",c[i][j]);
-	//		}
-	//		printf("\n");
-	//	} //잘 들어감. 
+	memset(checked, 0, sizeof(checked));
 
-	q.push(make_pair(0, 0));
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			if (checked[i][j] == 0) {
 
-	checked[0][0] = 1;
-	int x, y;
-	char tmp = c[0][0];
-	color[0][0] = 1;
-	redgreen[0][0] = 1;
-	while (!q.empty()) {
-		x = q.front().first;
-		y = q.front().second;
-		q.pop();
-
-		for (i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-
-			if (nx < 0 || nx >= n || ny < 0 || ny >= n)
-				continue;
-			if (checked[nx][ny] == 1)
-				continue;
-			if (c[x][y] != c[nx][ny] && color[nx][ny] == 0) {
-				//checked[nx][ny]=1;
-				//tmp=c[nx][ny];
-				color[nx][ny] = color[x][y] + 1;
-				//q.push(make_pair(nx,ny));
-
+				bfs(i, j, false);
+				a++;
 			}
-			else if (c[x][y] == c[nx][ny] && color[nx][ny] == 0) {
-				color[nx][ny] = color[x][y];
-				//checked[nx][ny]=1;
-			}
-			if ((c[x][y] == 'R' && c[nx][ny] == 'G') || (c[x][y] == 'G' && c[nx][ny] == 'R') || c[x][y] == c[nx][ny]) { //적록색약  
-				redgreen[nx][ny] = redgreen[x][y];
-			}
-			else {
-				redgreen[nx][ny] = redgreen[x][y] + 1;
-			}
-			checked[nx][ny] = 1;
-			q.push(make_pair(nx, ny));
 		}
-
 	}
 
+	memset(checked, 0, sizeof(checked));
+
 	for (i = 0; i < n; i++) {
-		for (j = 0; j < n; j++)
-			printf("%d ", color[i][j]);
-		printf("\n");
+		for (j = 0; j < n; j++) {
+			if (checked[i][j] == 0) {
+
+				bfs(i, j, true);
+				b++;
+			}
+		}
 	}
-	printf("\n \n");
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n; j++)
-			printf("%d ", redgreen[i][j]);
-		printf("\n");
-	}
-	printf("%d \n", color[n - 1][n - 1]);
-	printf("%d \n", redgreen[n - 1][n - 1]);
-	//	printf("%d",answer);
+
+	printf("%d \n", a);
+	printf("%d \n", b);
+
 	return 0;
+
 }
