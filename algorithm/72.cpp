a36bed917cb260c60f2f31245c304c7279957c21
wlwpfh <0112jyoung@naver.com>
2021-12-13 23:15:08 +0900

    공주 구하기 - 큐 활용

--- algorithm/72.cpp
+++ algorithm/72.cpp
@@ -4,47 +4,27 @@
 using namespace std;
 
 int main() {
-	int i, n, k, sum = 0;
-	queue<int> Q;
+	int i, n, k;
+	queue<int> q;
 	scanf("%d %d", &n, &k);
 	vector<int>check(n + 1);
+	int tmp, count = 0;
 
-	int count = 0, past, num, curr = k;
-	check[k] = 1;
+	for (i = 1; i <= n; i++)
+		q.push(i);
 
-	Q.push(k);
-	while (1) {
-		count = 0;
-		if (Q.size() == n - 1)
-			break;
-		if (k + curr > n) {
-			past = curr;
-			curr = (k + curr) % n;
-			printf("curr: %d \n", curr);
+	while (q.size() != 1) {
+		tmp = q.front();
+		q.pop();
+		count++;
+
+		if (count == k) {
+			count = 0;
 		}
 		else {
-			past = curr;
-			curr = curr + k;
+			q.push(tmp);
 		}
-		//	printf("현재 당번: %d \n",curr);
-		for (i = past + 1; i < curr; i++) {
-			if (check[i] == 1) {
-				count++;
-			}
-
-		}
-		printf("%d와 %d사이에 %d개의 체크된 값이 있다 \n", past + 1, curr, count);
-		if (check[curr + count] == 0) {
-			check[curr] = 1;
-			Q.push(curr);
-			printf("들어간 값: %d \n", curr);
-
-		}
-
 	}
-	for (i = 1; i < check.size(); i++) {
-		if (check[i] == 0)
-			printf("%d ", i);
-	}
+	printf("%d", q.front());
 	return 0;
 }
