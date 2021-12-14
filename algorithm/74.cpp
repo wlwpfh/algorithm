3654887e576c41200573d6403e72644fa6243573
wlwpfh <0112jyoung@naver.com>
2021-12-14 13:19:30 +0900

    최소 힙 - 우선순위 큐 사용

--- algorithm/74.cpp
+++ algorithm/74.cpp
@@ -6,28 +6,33 @@ using namespace std;
 int main() {
 
 	int n, top;
-	priority_queue<int, vector<int>, greater<int> > pq;
+	priority_queue<int> pq;
 	vector<int> v;
 	while (1) {
 		scanf("%d", &n);
 
 		if (n == 0) {
-			top = pq.top();
-			//pq.pop();
-			v.push_back(top);
+			if (pq.empty())
+				v.push_back(-1);
+			else {
+				top = pq.top();
+				pq.pop();
+				v.push_back(top);
+			}
+
 		}
 		else if (n == -1)
 			break;
 
 		else {
-			pq.push(n);
+			pq.push(-n);
 		}
 
 
 	}
 
 	for (int i = 0; i < v.size(); i++)
-		printf("%d \n", v[i]);
+		printf("%d \n", -v[i]);
 
 
 	return 0;
