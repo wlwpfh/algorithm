5a78c9697ca0260fd754a1a2a87fd323b186f4eb
wlwpfh <0112jyoung@naver.com>
2021-12-27 17:26:28 +0900

    11279 - 최대 힙

---
+++ algorithm/11279.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<queue>
+using namespace std;
+
+int main() {
+
+	int n, tmp;
+	priority_queue<int> pq;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &tmp);
+		if (tmp != 0) {
+			pq.push(tmp);
+		}
+		else {
+			if (pq.empty()) {
+				printf("0 \n");
+			}
+			else {
+				printf("%d \n", pq.top());
+				pq.pop();
+			}
+		}
+	}
+
+	return 0;
+}
