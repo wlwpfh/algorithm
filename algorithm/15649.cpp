7bb67abb8ee29dc64289f17aa16d9b5bf961058f
wlwpfh <0112jyoung@naver.com>
2022-01-02 22:50:42 +0900

    15649 - n과 m (1) (backtracking)

--- algorithm/15649.cpp
+++ algorithm/15649.cpp
@@ -1,20 +1,30 @@
 #include<stdio.h>
-
+#include<string.h>
 using namespace std;
+
 int n, m;
 
-// 1. 상태 공간 트리의 깊이 우선 검색을 실시
-// 2. 각 노드가 유망한지를 점검한다. 
-// 3. 해당 노드가 유망하지 않다면 부모로 돌아가서 검색을 계속한다. 
-
-void backTracking(int start, int count) {
-	//현재의 시작점, 내가 센 것의 개수 
-	for (int i = count; i <= m; i++) {
-		backTracking(start + 1, count + 1);
-		printf("%d ", start);
-	}
-	if (start == n && count == m)
+bool visit[10] = { 0, };
+int arr[10] = { 0, };
+void backTracking(int count) {
+	//현재 숫자, 해당 뽑은 개수  
+	if (count == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
 		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (visit[i] == false) {
+			visit[i] = true;
+			arr[count] = i;
+			backTracking(count + 1);
+			visit[i] = false;
+		}
+	}
+
 
 }
 
@@ -22,7 +32,9 @@ int main() {
 
 	scanf("%d %d", &n, &m);
 
-	backTracking(1, 1);
+
+	backTracking(0);
+
 
 	return 0;
 }
