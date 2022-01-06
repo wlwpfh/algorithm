9fe302d338556a1bd825b4be6d63c0ecab2aa471
wlwpfh <0112jyoung@naver.com>
2022-01-06 13:45:30 +0900

    하는 중

--- algorithm/2042.cpp
+++ algorithm/2042.cpp
@@ -1,18 +1,37 @@
 #include<stdio.h>
-
+#define MAX_SIZE 1000000
 using namespace std;
 
-long long lla[1000001];
+long long n, m, k, b;
+long long lla[MAX_SIZE];
+// 출력 자체는 long을 넘지 않는다.  
+
+void init() {
+	// 트리 안에 값 집어넣기 
+	for (int i = b - 1; i > 0; i--) {
+		lla[i] = (lla[i << 1] + lla[(i << 1) + 1]);
+	}
+}
+
+void update() {
+	//자기 위에 배열들 업데이트시키기 
+}
 
 int main() {
-	int n, m, k, tmp;
+	int tmp;
 	long long answer;
 	scanf("%d %d %d", &n, &m, &k);
 
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &tmp);
-		lla[i] = tmp;
+	for (b = 1; b < n; b <<= 1);
+	//b의 값이 8이 되어서 해당 값이 된다. 
+
+	for (int i = b; i < n + b; i++) {
+		scanf("%lld", &lla[i]);
 	}
+
+	init();
+
+
 
 	printf("%lld", answer);
 	return 0;
