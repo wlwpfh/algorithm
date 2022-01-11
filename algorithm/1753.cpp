80e85311247c9147ccdba334c554f942ff615763
wlwpfh <0112jyoung@naver.com>
2022-01-11 13:26:38 +0900

    1753 - 최단거리 (dijkstra)

--- algorithm/1753.cpp
+++ algorithm/1753.cpp
@@ -1,32 +1,81 @@
 #include<stdio.h>
+#include<queue>
 #include<vector>
-using namespace std;
 
-struct Node {
-	int c1;
-	int c2;
+#define MAX_SIZE 20000
+#define INF 987654321
+
+struct Data {
+	int node;
 	int weight;
 
-	Node(int a, int b, int c) {
-		c1 = a;
-		c2 = b;
-		weight = c;
+	Data(int a, int b) {
+		node = a;
+		weight = b;
+	}
+
+	bool operator<(const Data d) const {
+		return weight > d.weight;
 	}
 };
 
-int v, e, k;
-vector<Node> city;
+using namespace std;
+
+bool isVisited[MAX_SIZE + 1] = { false, };
+vector<Data> vec[MAX_SIZE + 1];
+int dis[MAX_SIZE + 1] = { 0, };
+priority_queue<Data> pq;
+
+int V, E, K;
 
 int main() {
-	int a, b, c;
 
-	scanf("%d %d", &v, &e);
-	scanf("%d", &k);
+	scanf("%d %d", &V, &E);
 
-	for (int i = 0; i < e; i++) {
-		scanf("%d %d %d", &a, &b, &c);
-		city.push_back(Node(a, b, c));
+	scanf("%d", &K); //출발 지점
+
+	int u, v, w;
+
+	for (int i = 0; i < E; i++) {
+		scanf("%d %d %d", &u, &v, &w);
+
+		vec[u].push_back(Data(v, w));
+		//방향 그래프이니까 일방향으로 설정하기  
 	}
-	//양방향 X
 
+	for (int i = 1; i <= V; i++) {
+		dis[i] = INF;
+	}
+
+	dis[K] = 0;
+	pq.push(Data(K, 0));
+
+	while (!pq.empty()) {
+		Data now = pq.top();
+		pq.pop();
+
+		if (isVisited[now.node] == true)
+			continue;
+
+		isVisited[now.node] = true;
+
+		for (int i = 0; i < vec[now.node].size(); i++) {
+			Data next = vec[now.node].at(i);
+
+			if (dis[next.node] > dis[now.node] + next.weight) {
+				dis[next.node] = dis[now.node] + next.weight;
+
+				pq.push(Data(next.node, dis[next.node]));
+			}
+		}
+	}
+
+	for (int i = 1; i <= V; i++) {
+		if (dis[i] == INF)
+			printf("INF \n");
+		else
+			printf("%d \n", dis[i]);
+	}
+
+	return 0;
 }
