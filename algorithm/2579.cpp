3484c4e416e3410c8c4aa693b3b82077a0f69d63
wlwpfh <0112jyoung@naver.com>
2022-01-13 21:03:14 +0900

    하는 중

---
+++ algorithm/2579.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<algorithm>
+#define MAX_SIZE 300
+using namespace std;
+
+int stair[MAX_SIZE + 1];
+int dp[MAX_SIZE + 1];
+
+int main() {
+	int n;
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &stair[i]);
+	}
+
+	dp[0] = 0; //¶¥¹Ù´Ú
+	dp[1] = stair[1];
+	dp[2] = stair[1] + stair[2];
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = max(dp[i - 3] + stair[i - 1], dp[i - 2] + stair[i]);
+		//ÇÑ¹ø¿¡ ÇÑÄ­¸¸ °¡°Å³ª µÎÄ­¸¸ °¡°Å³ª
+		// dp[3]=dp[0]+stair[2] -> µÎÄ­ °¡±â
+		// dp[3]=dp[1]+stair[3] -> ÇÑÄ­°¡°í 3¹øÂ°Ä­ °¡±â 
+	}
+
+	printf("%d", dp[n]);
+
+	return 0;
+}
