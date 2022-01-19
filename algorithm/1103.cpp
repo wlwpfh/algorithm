eb7bf60c94a623137b769a807b54a60613f0c59c
wlwpfh <0112jyoung@naver.com>
2022-01-19 14:26:56 +0900

    1103 - 게임

--- algorithm/1103.cpp
+++ algorithm/1103.cpp
@@ -1,56 +1,65 @@
 #include<stdio.h>
-
+#include<algorithm>
 using namespace std;
 
-int n, m;
+int N, M, Ans;
 int dp[55][55] = { 0, }; //계산 중복을 막기 위해서  
 char map[55][55] = { 0, }; //지도를 저장하기 위해서  
 bool visited[55][55]; // 갔는지 안갔는지 체크하기 위해서  
-int max_level = -1;
-
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
 
-void go(int x, int y, int level) {
+bool go(int r, int c, int cnt)
+{
+    //무한번 움직일 수 있는 경우
+    if (Ans == -1) return true;
 
-	visited[x][y] = true;
+    Ans = max(Ans, cnt);
 
-	dp[x][y] = level;
+    visited[r][c] = true;
+    //DP테이블에 계산 결과를 update하여 더 유리 한것이 계산된 것이라면 계산하지 않도록 한다.
+    dp[r][c] = cnt;
 
-	for (int i = 0; i < 4; i++) {
-		int next_x = x + dx[i] * (map[x][y] - '0'); //이렇게 하면 int가 된다 
-		int next_y = y + dy[i] * (map[x][y] - '0');
+    for (int i = 0; i < 4; i++)
+    {
+        int nr = r + dx[i] * (map[r][c] - '0');//문자열로 된 숫자에서 '0' 문자를 빼면 int가 된다.(ASCII코드)
+        int nc = c + dy[i] * (map[r][c] - '0');
 
-		if (next_x < 0 || next_x >= n || next_y < 0 || next_y >= m || map[next_x][next_y] == 'H') {
-			if (max_level < level)
-				max_level = level;
-			return;
-		}
-		//범위에 벗어나는 경우  
-		if (visited[next_x][next_y]) {
-			max_level = -1;
-			return;
-		} //이미 왔던 경우  
+        //매트릭스 범위를 벗어나거나 구멍인것은 게임이 종료되므로 탐색을 진행하지 않는다.
+        if (nr < 0 || nr >= N || nc < 0 || nc >= M || map[nr][nc] == 'H') continue;
 
+        //방문한 곳에 또 방문하려 한다면 무한번 움직일 수 있다는 의미
+        if (visited[nr][nc])
+        {
+            Ans = -1;
+            return true;
+        }
 
-		go(next_x, next_y, level + 1);
+        //이미 계산된 것은 DP테이블에 기록되며 DP의 값이 cnt보다 크다면 
+        //이미 더 길게 움직이는 것이 계산되어 있다.
+        if (dp[nr][nc] > 0 && cnt < dp[nr][nc]) continue;
 
-	}
+        //무한번 움직일 수 있다면 dfs에서 true가 return된다
+        if (go(nr, nc, cnt + 1)) return true;
+    }
 
+    //돌아와서는 Visit Flag를 false로 설정
+    visited[r][c] = false;
+    return false;
 }
-
 
 int main() {
 
-	scanf("%d %d", &n, &m);
+    scanf("%d %d", &N, &M);
 
-	for (int i = 0; i < n; i++) {
-		scanf("%s", &map[i]);
+    for (int i = 0; i < N; i++) {
+        scanf("%s", &map[i]);
 
-	}
+    }
 
-	go(0, 0, 1);
+    go(0, 0, 1);
 
-	printf("%d", max_level);
-	return 0;
+    printf("%d", Ans);
+
+    return 0;
 }
