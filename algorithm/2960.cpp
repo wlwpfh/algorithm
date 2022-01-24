94e397f07985ab126098cf0031ab40c1e4526f19
wlwpfh <0112jyoung@naver.com>
2022-01-24 14:20:57 +0900

    2960 - 에라토스테네스의 체

--- algorithm/2960.cpp
+++ algorithm/2960.cpp
@@ -4,23 +4,50 @@
 using namespace std;
 
 int main() {
-	int n, k;
+	int n, k, answer;
 	scanf("%d %d", &n, &k);
-
+	int count = 0;
 	int arr[n + 1];
 
 	int i, j;
 	for (i = 2; i <= n; i++)
 		arr[i] = i;
 
-	for (i = 2; i <= sqrt(n); i++) {
+	for (i = 2; i <= n; i++) {
 		if (arr[i] == 0)
 			continue;
 
-		for (j = i * i; j <= n; j += i)
-			arr[j] = 0;
-		printf("i: %d ,j:%d, arr[j]=%d \n", i, j, arr[j]);
+		arr[i] = 0;
+		count++;
+		//printf("지워진 수는 %d이다 \n",i);
+		if (count == k) {
+			answer = i;
+			break;
+		}
+
+		for (j = i * i; j <= n; j += i) {
+			if (arr[j] == 0)
+				continue;
+			else {
+				arr[j] = 0;
+				//printf("지워진 수는 %d이다 \n",j);
+				count++;
+			}
+
+			if (count == k) {
+				answer = j;
+				break;
+			}
+		}
+
+
+		if (answer != 0)
+			break;
+
+
 	}
+
+	printf("%d", answer);
 
 	return 0;
 }
