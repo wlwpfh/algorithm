b264824c0c2ee8b4170161f2c83e9b3c349eeed0
wlwpfh <0112jyoung@naver.com>
2022-01-28 15:42:22 +0900

    1932 - 정수 삼각형

--- algorithm/1932.cpp
+++ algorithm/1932.cpp
@@ -2,11 +2,13 @@
 #include<algorithm>
 using namespace std;
 //왼쪽과  오른쪽인지 확인하기  
-int arr[501][501];
+int arr[501][501] = { 0, };
 int dp[501][501] = { 0, };
+int n;
+
 int main() {
 
-	int i, j, n;
+	int i, j, sum = 0;
 	scanf("%d", &n);
 	int answer;
 	for (i = 0; i < n; i++) {
@@ -14,60 +16,39 @@ int main() {
 			scanf("%d", &arr[i][j]);
 		}
 	}
+
 	dp[0][0] = arr[0][0];
-	for (i = 1; i < n; i++) {
-		for (j = 0; j < n - 1; j++) {
-			dp[i][j] = dp[i - 1][j] + max(arr[i - 1][j], arr[i - 1][j + 1]);
-			printf("dp[%d][%d]=%d \n", i, j, dp[i][j]);
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j <= i; j++) {
+			if (j == 0) {
+				dp[i][j] = arr[i][j] + dp[i - 1][j];
+			}
+			else if (j == i) {
+				dp[i][j] = arr[i][j] + dp[i - 1][j - 1];
+			}
+			else {
+				//비교해서 더 큰 값으로 
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + arr[i][j];
+			}
+
 		}
-
 	}
 
-	int max = 0;
-	for (i = 0; i < 501; i++) {
-		if (max < dp[n - 1][i])
-			max = dp[n - 1][i];
-	}
-	printf("%d", max);
 
+	for (i = 0; i < n; i++) {
+		for (j = 0; j <= i; j++) {
+			printf("%d ", dp[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (i = 0; i < n; i++) {
+		if (dp[n - 1][i] > sum)
+			sum = dp[n - 1][i];
+	}
+
+	printf("%d", sum);
 
 	return 0;
 }
-
-//#include<stdio.h>
-//#include<algorithm>
-//using namespace std;
-////왼쪽과  오른쪽인지 확인하기  
-//int arr[501][501];
-//int dp[501][501] = { 0, };
-//int sum = 0, result = 0;
-//int max_sum(int left, int right) {
-//	if (sum > result)
-//		result = sum;
-//	if (left == n - 1)
-//		return result;
-//	if (right == 0 || right == n - 1) {
-//
-//	}
-//
-//
-//}
-//
-//int main() {
-//
-//	int i, j, sum = 0;
-//	scanf("%d", &n);
-//	int answer;
-//	for (i = 0; i < n; i++) {
-//		for (j = 0; j <= i; j++) {
-//			scanf("%d", &arr[i][j]);
-//		}
-//	}
-//
-//
-//
-//	printf("%d", sum);
-//
-//
-//	return 0;
-//}
