214d65664b2b2c1ce341ddcd831bb835645e6b38
wlwpfh <0112jyoung@naver.com>
2022-02-16 15:12:41 +0900

    2108 - 통계학

--- algorithm/2108.cpp
+++ algorithm/2108.cpp
@@ -3,40 +3,55 @@
 #include<algorithm>
 #include<cmath>
 using namespace std;
-int count[8001];
+int counts[8001] = { 0, };
+vector<int> v;
+
+//첫번째는 count개수, 두번째는 값  
 int main() {
-	int n, freq = 0, count = 0, num = 0;
+	int n, freq = 1, num = 0;
 	scanf("%d", &n);
 	int i, sum = 0;
 	vector<int>arr(n);
+	int freq_num = 0;
 	for (i = 0; i < n; i++) {
 		scanf("%d", &arr[i]);
 		sum += arr[i];
-		if (arr[i] >= 0) {
-			count[arr[i] + 4000]++;
+
+		counts[arr[i] + 4000]++;
+
+		if (counts[arr[i] + 4000] >= freq) {
+			freq = counts[arr[i] + 4000];
+
 		}
-		else
-			count[arr[i] - 4000]++;
+
 	}
 	sort(arr.begin(), arr.end());
 
-	int freq = 0, num = 0;
 
-	for (i = 0; i < 8001; i++) {
-		if (count[i] > freq) {
-			freq = count[i];
-			num = i + 4000;
+
+	bool isSecond = false;
+
+	for (i = 0; i <= 8000; i++) {
+		if (counts[i] == freq) {
+
+			freq_num = i - 4000;
+
+			if (isSecond)
+				break;
+
+
+			isSecond = true;
 		}
 	}
 
 
-	printf("%.0f \n", round((double)sum / (double)n));
-	printf("%d \n", arr[n / 2]);
 
-	printf("%d \n", arr[n - 1] - arr[0]);
+	printf("\n1. %.0f \n", round((double)sum / (double)n));
+	printf("2. %d \n", arr[n / 2]);
+	printf("3. %d \n", freq_num);
+	printf("4. %d \n", arr[n - 1] - arr[0]);
 
-	arr.clear()
-		arr.shrink_to_fit();
+
 
 	return 0;
 }
