6bb9c756179d70db51fca9b225c1bd9b8cb4200b
wlwpfh <0112jyoung@naver.com>
2022-02-20 21:46:47 +0900

    하는 중

--- algorithm/18405.cpp
+++ algorithm/18405.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 queue<pair<int, int> > q;
 int map[MAX_N + 1][MAX_N + 1] = { 0, };
-int checked[MAX_N + 1][MAX_N + 1] = { 0, };
 int visited[MAX_N + 1][MAX_N + 1] = { 0, };
 int N, K, S, X, Y;
 //N * N 행렬, 총 K개의 바이러스, S초 후의 X,Y에 있는 바이러스 종류 출력 
@@ -25,17 +24,14 @@ struct Start {
 		y = b;
 		virus = c;
 	}
-	bool operator<(Start s) {
-		return virus < s.virus;
-	}
 };
 
 vector<Start> point;
 
-void bfs(int a, int b) {
+void bfs(int a, int b, int time) {
 	q.push(make_pair(a, b)); //위치를 받음  
-	checked[a][b] = 1;
-	visited[a][b] = 1;
+	//checked[a][b]=1;
+	//visited[a][b]=time;
 	int j;
 	int virus = map[a][b];
 	int x = q.front().first;
@@ -49,14 +45,10 @@ void bfs(int a, int b) {
 		if (nx < 0 || nx >= N || ny < 0 || ny >= N) //범위를 벗어난 경우 
 			continue;
 		if (map[nx][ny] == 0) {
-			checked[nx][ny] = 1;
+			//checked[nx][ny]=1;
 			map[nx][ny] = map[x][y];
+			visited[nx][ny] = time + 1;
 			//printf("(%d, %d)에도 바이러스(%d)전염 \n",nx,ny,virus); 
-		}
-		else if (map[nx][ny] > map[x][y] && checked[nx][ny] == 1) {
-			//printf("(%d, %d)에 바이러스(%d) -> 바이러스(%d) 전염 \n",nx,ny,map[nx][ny],virus); 	 
-			map[nx][ny] = map[x][y];
-
 		}
 
 	}
@@ -77,47 +69,36 @@ int main() {
 	scanf("%d %d", &N, &K);
 
 	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++)
+		for (j = 0; j < N; j++) {
 			scanf("%d", &map[i][j]);
 
-		if (map[i][j] != 0)
-			visited[i][j] = 1;
-		//point.push_back(Start(i,j,map[i][j]));
+			if (map[i][j] != 0)
+				visited[i][j] = 1;
+		}
+
 	}
 
 	scanf("%d %d %d", &S, &X, &Y);
 
 
-	for (int k = 0; k < S; k++) {
-		printf("%d초 |||||||| \n", k + 1);
+	for (int k = 1; k <= S; k++) {
+		//printf("%d초 |||||||| \n",k+1);
 		for (i = 0; i < N; i++) {
 			for (j = 0; j < N; j++) {
 
-				if (checked[i][j] == 0 && map[i][j] != 0) {
-					printf("%d번 바이러스 %d로 감염 시작 !!!!\n", count + 1, map[i][j]);
-					bfs(i, j);
-					count++;
-
+				if (map[i][j] != 0 && visited[i][j] == k) {
+					//printf("%d번 바이러스 %d로 감염 시작 !!!!\n",count+1,map[i][j]);
+					bfs(i, j, k);
 
 				}
 
 			}
 
-
 		}
 	}
 
 
-
-
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++) {
-			printf("%d ", map[i][j]);
-		}
-		printf("\n");
-	}
-
-	printf("%d", map[X][Y]);
+	printf("%d", map[X - 1][Y - 1]);
 
 
 	return 0;
