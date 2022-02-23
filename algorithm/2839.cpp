15816cfc77e5a381d238228bceec499a01eb13cd
wlwpfh <0112jyoung@naver.com>
2022-02-23 21:46:18 +0900

    하는 중

--- algorithm/2839.cpp
+++ algorithm/2839.cpp
@@ -1,36 +1,32 @@
-#include<stdio.h> 
-
+#include<stdio.h>
+#include<algorithm>
 using namespace std;
-int n, count = 0, min = 100000;
-int arr[100];
-int left_over(int num) {
-	if (num < 0)
-		return -1;
-	else if (num == 0) {
-		if (min > count)
-			min = count;
-		count = 0;
-		return min;
-	}
-	else {
-		if (num >= 3) {
-			arr[num - 3] = left_over(num - 3);
-			count++;
-		}
-		if (num >= 5) {
 
-			arr[num - 5] = left_over(num - 5);
-			count++;
-		}
+int N;
+int sugar[5001] = { 0,0,0,1,0,1,0 };
+
+int bongji(int number) {
+
+	if (sugar[number] > 0) {
+		return sugar[number];
 	}
+
+	if (number >= 3)
+		return sugar[number] += bongji(number - 3);
+	else if (number >= 5)
+		return sugar[number] += bongji(number - 5);
+
 }
 
+
 int main() {
-	scanf("%d", &n);
 
-	int answer = left_over(n);
-
-	printf("%d", answer);
+	scanf("%d", &N);
+	sugar[N] = -1;
+	if (sugar[N] == 0)
+		printf("-1");
+	else
+		printf("%d", sugar[N]);
 
 	return 0;
 }
