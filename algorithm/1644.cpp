f9a5fdd9964621dbf23e26dcb5f9ccd31aeb8807
wlwpfh <0112jyoung@naver.com>
2022-02-23 15:34:59 +0900

    1644 - 소수의 연속합 (소수, 투 포인터)

--- algorithm/1644.cpp
+++ algorithm/1644.cpp
@@ -1,27 +1,44 @@
 #include<stdio.h>
-
+#include<vector>
 using namespace std;
-int n, answer = 0, count = 0, K = 0;
-int arr[100001];
+int N, answer = 0, K = 0, sum = 0;
+int arr[4000001];
+vector<int> v;
 int main() {
-	scanf("%d", &n);
+	scanf("%d", &N);
 
-	for (int i = 2; i <= n; i++) {
+	for (int i = 2; i <= N; i++) {
 		arr[i] = i;
 	}
 
-	for (int i = 2; i <= n; i++) {
+	for (int i = 2; i * i <= N; i++) {
 		if (arr[i] == 0)
 			continue;
-		for (int j = 2 * i; j <= n; j += i) {
-			a[j] = 0;
-
-		}
+		for (int j = i + i; j <= N; j += i)
+			arr[j] = 0;
 	}
+	int left = 0, right = 0;
 
-	for (int i = 2; i < 100; i++) {
-		if (arr[i] != 0) {
+	for (int i = 2; i <= N; i++) {
+		if (arr[i] != 0)
+			v.push_back(i);
+	}// 소수만 넣기  
 
+	while (1) {
+		if (sum > N) {
+			sum -= v[left++];
+			//넣었던 왼쪽을 빼고
+			//왼쪽을 한칸 더 이동 
 		}
+		else if (right == v.size())
+			break;
+		//right가 끝까지 온 경우 (해당 수까지 온 경우)
+		else {
+			sum += v[right++];
+		}
+		if (sum == N)
+			answer++;
 	}
+	printf("%d", answer);
+	return 0;
 }
