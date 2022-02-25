c9d643e9fc191b2434103a18f30544455109da80
wlwpfh <0112jyoung@naver.com>
2022-02-25 21:39:33 +0900

    하는 중

--- algorithm/2839.cpp
+++ algorithm/2839.cpp
@@ -6,16 +6,13 @@ int N;
 int sugar[5001] = { 0,0,0,1,0,1,0 };
 
 int bongji(int number) {
-
+	if (number == N)
+		return sugar[number];
 	if (sugar[number] > 0) {
 		return sugar[number];
 	}
 
-	if (number >= 3)
-		return sugar[number] += bongji(number - 3);
-	else if (number >= 5)
-		return sugar[number] += bongji(number - 5);
-
+	return sugar[number] = min(bongji(number - 3) + 1, bongji(number - 5) + 1);
 }
 
 
@@ -23,10 +20,16 @@ int main() {
 
 	scanf("%d", &N);
 	sugar[N] = -1;
+
+	bongji(N);
+
 	if (sugar[N] == 0)
-		printf("-1");
+		printf("-1 \n");
 	else
-		printf("%d", sugar[N]);
+		printf("%d \n", sugar[N]);
+
+	for (int i = 0; i <= N; i++)
+		printf("%d ", sugar[i]);
 
 	return 0;
 }
