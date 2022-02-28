f333cb6ebc3c07f596f4594e7039599859fd53b6
wlwpfh <0112jyoung@naver.com>
2022-02-28 15:23:21 +0900

    2839 - 설탕배달 (dp)

--- algorithm/2839.cpp
+++ algorithm/2839.cpp
@@ -3,33 +3,28 @@
 using namespace std;
 
 int N;
-int sugar[5001] = { 0,0,0,1,0,1,0 };
-
-int bongji(int number) {
-	if (number == N)
-		return sugar[number];
-	if (sugar[number] > 0) {
-		return sugar[number];
-	}
-
-	return sugar[number] = min(bongji(number - 3) + 1, bongji(number - 5) + 1);
-}
-
+int sugar[5001];
 
 int main() {
 
 	scanf("%d", &N);
-	sugar[N] = -1;
 
-	bongji(N);
+	sugar[3] = sugar[5] = 1;
+
+	for (int i = 6; i <= N; i++) {
+		if (sugar[i - 3] != 0)
+			sugar[i] = sugar[i - 3] + 1;
+		if (sugar[i - 5] != 0)
+			if (sugar[i] == 0)
+				sugar[i] = sugar[i - 5] + 1;
+			else
+				sugar[i] = min(sugar[i], sugar[i - 5] + 1);
+	}
 
 	if (sugar[N] == 0)
-		printf("-1 \n");
+		printf("-1");
 	else
-		printf("%d \n", sugar[N]);
-
-	for (int i = 0; i <= N; i++)
-		printf("%d ", sugar[i]);
+		printf("%d", sugar[N]);
 
 	return 0;
 }
