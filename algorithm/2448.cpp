58e01baa2acf1b98ac733379841096b305da72cb
wlwpfh <0112jyoung@naver.com>
2022-03-06 01:02:51 +0900

    2448 - 별 찍기 11

--- algorithm/2448.cpp
+++ algorithm/2448.cpp
@@ -2,31 +2,29 @@
 
 using namespace std;
 
-char arr[3073][6145] = { ' ', };
+char arr[3100][6200] = { ' ', };
 
 void printStars(int x, int y) {
 	arr[x][y] = '*';
-	arr[x - 1][y + 1] = '*';
+	arr[x + 1][y - 1] = '*';
 	arr[x + 1][y + 1] = '*';
 
-	//	for(int i=x-2; i<=x+2;i++)
-	//		arr[i][y+2]='*';	
+	for (int i = y - 2; i <= y + 2; i++)
+		arr[x + 2][i] = '*';
 }
 
 
-void star(int i, int j, int num) { //xÁÂÇ¥, yÁÂÇ¥  
+void star(int i, int j, int num) { //x 
 	if (num == 3) {
 		printStars(i, j);
 		//°¡Àå ÀÛÀº »ï°¢Çü ±×¸®±â  
 		return;
 	}
-	else {
-		//²ÀÁþÁ¡ ÀÌµ¿ 
-		star(i, j, num / 2);
-		star(i + num / 2, j - num / 2, num / 2);
-		star(i + num / 2, j + num / 2, num / 2);
+	//²ÀÁþÁ¡ ÀÌµ¿ 
+	star(i, j, num / 2);
+	star(i + num / 2, j - num / 2, num / 2);
+	star(i + num / 2, j + num / 2, num / 2);
 
-	}
 
 
 }
@@ -38,11 +36,15 @@ int main() {
 	scanf("%d", &num);
 
 
-	star(num, 0, num);
+	star(0, num - 1, num);
 
-	for (i = 0; i <= num; i++) {
-		for (j = 0; j <= 2 * num; j++)
-			printf("%c ", arr[i][j]);
+	for (i = 0; i < num; i++) {
+		for (j = 0; j < 2 * num; j++) {
+			if (arr[i][j] == '*')
+				printf("*");
+			else
+				printf(" ");
+		}
 		printf("\n");
 	}
 
