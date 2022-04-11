f98887d39ab5f1980f67a4c49032d0c65ed4655f
wlwpfh <0112jyoung@naver.com>
2022-04-11 16:51:26 +0900

    1256 - 사전 (dp)

--- algorithm/1256.cpp
+++ algorithm/1256.cpp
@@ -1,90 +1,63 @@
-//#include<string>
-//#define MAX_SIZE 201
-//
-//using namespace std;
-//
-//int dp[MAX_SIZE][MAX_SIZE];
-//
-//int combi(int n, int r) {
-//	if (n == r)
-//		return dp[n][r] = 1;
-//	if (r == 1)
-//		return dp[n][r] = n;
-//
-//	return dp[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
-//}
-//
-//int main() {
-//	int n, m, k;
-//	string answer = "";
-//	scanf("%d %d %d", &n, &m, &k);
-//
-//	int res = combi(n + m - 1, m);
-//	int tmp_k = k;
-//	if (k > res * 2) {
-//		printf("-1");
-//		return 0;
-//	}
-//	else {
-//		while (answer.length() == k) {
-//			if (tmp_k <= res) {
-//				answer += "a";
-//				tmp_k -= 1;
-//				res = combi(n - 1 + m - 1, m);
-//			}
-//			else {
-//				answer += "z";
-//				tmp_k -= 1;
-//				res = combi(n + m - 1 - 1, m - 1);
-//			}
-//		}
-//	}
-//
-//	printf("%s", answer.c_str());
-//
-//
-//	return 0;
-//}
-
 #include<string>
 #define MAXN 100
 #define MAXK 1000000000
 using namespace std;
 
-int dp[MAXN * 2 + 1][MAXN];
+int dp[MAXN * 2 + 1][MAXN + 1];
+//n이 제일 클 것이고 n+m을 해봤자 2n과 같거나 작기 때문에 2maxn+1로 설정했음. 
 
 int main() {
 	int n, m, k;
 	scanf("%d %d %d", &n, &m, &k);
 	n += m;
-
+	//n은 a의 개수, m은 z의 개수
+	//현재 n은 a와 z의 개수를 합친 것  
 	dp[0][0] = 1;
 
 	for (int i = 1; i <= n; i++) {
 		dp[i][0] = 1;
-		for (int j = 1; j <= i && j <= m; j++) {
+		//조합할 때 iC0은 1이니까  
+		for (int j = 1; j <= n; j++) {
 			dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
 
 			if (dp[i][j] > MAXK)
 				dp[i][j] = MAXK;
+			//해당 수가 넘어갈 경우  
 		}
-	}
+	} //조합으로 푸는 것  
+	//nCr= n-1Cr-1 +n-1Cr;
+	//a와 z를 이용하여 만들어낼 수 있는 가지수  
 
 	if (dp[n][m] < k) {
 		printf("-1 \n");
 		return 0;
+	}//문자열의 길이가 k보다 안되는 경우
+	//다 했는데 만들 수 있는 가지수가 더 적은 경우   
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j <= n; j++)
+			printf("%d ", dp[i][j]);
+		printf("\n");
 	}
 
-	while (n > 0) {
+	while (n) {
+		//길이가 n이니까 문자 하나씩 출력
+		//k번째 문자열   
 		if (dp[n - 1][m] >= k)
 			printf("a");
+		//
 		else {
 			printf("z");
 			k -= dp[n - 1][m--];
+
 		}
 
 
 		n--;
+		//길이를 하나씩 줄여나간다.  
 	}
+
+
+
 	return 0;
 }
