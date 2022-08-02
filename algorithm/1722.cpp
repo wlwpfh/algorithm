1bcb0520acdb28407ae4ab84a02bb73e57c6ce88
yangjiyoung <0112jyoung@naver.com>
2022-08-02 23:40:32 +0900

    하는 중

--- algorithm/1722.cpp
+++ algorithm/1722.cpp
@@ -2,25 +2,41 @@
 
 using namespace std;
 
-int arr[21];
-bool visited[21];
-int result[21];
-int N, k, count, tmp;
+int N, tmp, ans_2, count;
+long long k;
+int arr[20], ans[20], result[20];
+bool visited[20];
 
 void DFS(int num) {
-	if (count == tmp || num == N + 1) {
+	if (num == N) {
+		count++;
+		for (int i = 0; i < N && k == 2; i++) {
+
+			if (ans[i] == result[i])
+				tmp++;
+			else
+				break;
+
+		}
+
+		if (count == tmp) {
+			for (int i = 0; i < N; i++)
+				result[i] = ans[i];
+		} //1이 입력되었을 때  
+		if (tmp == N) {
+			ans_2 = count;
+		}//2가 입력되었을 때  
 		return;
 	}
-	printf("num=%d \n", num);
-	for (int i = 1; i <= N; i++) {
-		if (visited[i] == false) {
+
+	for (int i = 0; i < N; i++) {
+		if (!visited[i]) {
 			visited[i] = true;
-			result[num] = arr[i];
+			ans[num] = arr[i];
 			DFS(num + 1);
 			visited[i] = false;
 		}
 	}
-	count++;
 }
 
 int main() {
@@ -28,23 +44,33 @@ int main() {
 
 	scanf("%d", &N);
 
-	scanf("%d", &k);
+	for (i = 0; i < N; i++)
+		result[i] = 0;
+
+	scanf("%lld", &k);
 
 	if (k == 1) {
 		scanf("%d", &tmp);
+		for (i = 0; i < N; i++)
+			arr[i] = i + 1;
 	}
 	else {
-		for (i = 0; i < N; i++)
-			scanf("%d", &arr[i]);
+
+		for (i = 0; i < N; i++) {
+			scanf("%d", &result[i]);
+			arr[i] = i + 1;
+
+		}
 	}
 
+	DFS(0);
+
 	if (k == 1) {
-		DFS(1);
-		for (i = 1; i <= N; i++)
+		for (i = 0; i < N; i++)
 			printf("%d ", result[i]);
 	}
 	else {
-		;
+		printf("%d", ans_2);
 	}
 
 	return 0;
