80ee975102972ff39437b2e9ad9183a3ba52626d
yangjiyoung <0112jyoung@naver.com>
2022-08-16 17:26:30 +0900

    타켓넘버 (DFS)

--- algorithm/targetnumber.cpp
+++ algorithm/targetnumber.cpp
@@ -1,37 +1,30 @@
 #include <string>
 #include <vector>
-#include <queue>
+
 using namespace std;
-int cal[2] = { 1,-1 }; //곱하면 된다.
-int solution(vector<int> numbers, int target) {
-    int i, j, curr, sum, answer = 0;
 
-    queue<int>Q;
-    Q.push(numbers[0]);
-    vector<int> check(numbers.size()); //numbers다 넣었는지 확인
-    check[0] = 1;
-    while (!Q.empty()) {
-        curr = Q.front();
-        Q.pop();
-
-        for (i = 1; i < numbers.size(); i++) {
-            for (j = 0; j < 2; j++) {
-                sum = curr + (numbers[i]) * cal[j];
-
-                if (sum == target) {
-                    break;
-                }
-                else {
-                    answer++;
-                    check[i] = 1;
-                    Q.push(sum);
-
-                }
-            }
-        }
-
-
+int num_size, answer = 0, goal;
+vector<int> tmp;
+void DFS(int index, int sum) {
+    if (index == num_size) {
+        if (sum == goal)
+            answer++;
+        return;
     }
+
+    DFS(index + 1, sum + tmp[index]);
+    DFS(index + 1, sum - tmp[index]);
+
+}
+
+int solution(vector<int> numbers, int target) {
+    goal = target;
+    num_size = numbers.size();
+
+    tmp = numbers;
+    //초기화 
+
+    DFS(0, 0);
 
     return answer;
 }
