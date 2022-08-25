408b0ca2be2586fdecaa888972509c67f621dbec
yangjiyoung <0112jyoung@naver.com>
2022-08-25 23:36:42 +0900

    2206 - 벽부수고이동하기 (bfs)

--- algorithm/2206.cpp
+++ algorithm/2206.cpp
@@ -1,87 +1,69 @@
-#include<stdio.h>
+#include<iostream>
+#include<string>
 #include<queue>
-#define INF 987654321
 using namespace std;
 
-int N, M;
-int map[1000][1000];
-queue<pair<int, int > > q;
-queue<pair<int, int> >wall;
-int move;
-bool meet_end;
+struct Wall {
+	int x, y, crushed;
+};
 
-void Move() {
-	int dx[] = { -1,1,0,0 };
-	int dy[] = { 0,0,-1,1 };
+int N, M, answer = -1;
+char map[1000][1000];
+int dis[1000][1000][2] = { 0, };
+int dx[] = { -1,1,0,0 }, dy[] = { 0,0,-1,1 };
 
-	int move = 0;
-
-	q.push(make_pair(0, 0));
-	map[0][0] = 1;
+int Move() {
+	queue<Wall> q;
+	dis[0][0][0] = 1;
+	q.push({ 0,0,0 });
 
 	while (!q.empty()) {
-		pair<int, int> p = q.front();
+
+		Wall now = q.front();
+		int x = now.x, y = now.y, crush = now.crushed;
+
 		q.pop();
 
-		for (int i = 0; i < 4; i++) {
-			int nx = dx[i] + p.first;
-			int ny = dy[i] + p.second;
+		if (x == N - 1 && y == M - 1) {
 
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+			return dis[now.x][now.y][now.crushed];
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || ny < 0 || nx >= N || ny >= M) //범위 밖  
 				continue;
-			if (map[nx][ny] == 0) {
-				map[nx][ny] = map[p.first][p.second] + 1;
-				q.push(make_pair(nx, ny));
+
+			if (map[nx][ny] == '1' && crush == 0) { //벽이고 벽을 부순 적이 없다면  
+				q.push({ nx,ny,crush + 1 }); //한번 부숨  
+				dis[nx][ny][crush + 1] = dis[x][y][crush] + 1;
+			}
+			else if (map[nx][ny] == '0' && dis[nx][ny][crush] == 0) { //가본적이 없는곳
+
+				q.push({ nx,ny,crush });
+				dis[nx][ny][crush] = dis[x][y][crush] + 1;
 			}
 		}
 	}
-}
-
-void fill_zero() {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++)
-			if (map[i][j] != -1)
-				map[i][j] = 0;
-	}
+	return -1;
 }
 
 int main() {
-	int i, j, min;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		string s;
+		cin >> s;
 
-	scanf("%d %d", &N, &M);
+		for (int j = 0; j < M; j++)
+			map[i][j] = s[j];
 
-	for (i = 0; i < N; i++)
-		for (j = 0; j < M; j++) {
-			scanf("%1d", &map[i][j]);
-
-			if (map[i][j] == 1) {
-				wall.push(make_pair(i, j));
-				map[i][j] = -1;
-			}
-		}
-
-
-	min = INF;
-
-	while (!wall.empty()) {
-		pair<int, int> now = wall.front();
-		wall.pop();
-
-		map[now.first][now.second] = 0;
-		Move();
-
-		meet_end = map[N - 1][M - 1] != 0 ? true : false;
-
-		if (meet_end == true)
-			min = min > map[N - 1][M - 1] ? map[N - 1][M - 1] : min;
-
-		map[now.first][now.second] = -1;
-		fill_zero();
 	}
 
-	if (min != INF)
-		printf("%d", min);
-	else
-		printf("-1");
+
+
+	cout << Move() << endl;
+
 	return 0;
 }
