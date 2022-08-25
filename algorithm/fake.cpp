67e3bafb9ea032d370c79beb713449351ae457cb
yangjiyoung <0112jyoung@naver.com>
2022-08-25 23:35:20 +0900

    위장 (hash/unordered map)

--- algorithm/fake.cpp
+++ algorithm/fake.cpp
@@ -1,23 +1,18 @@
 #include <string>
-#include <vector>
 #include <unordered_map>
-#include <algorithm>
+
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
-    int i, j, answer = 0;
-    unordered_map<string, string> com(clothes.size()); //하나만 올 수 있음.
-    vector<string> kind;
-
+    int i, j, answer = 1;
+    unordered_map<string, int> com(clothes.size());
 
     for (i = 0; i < clothes.size(); i++) {
-        com[clothes[i][1]] = clothes[i][0];
-        kind.push_back(clothes[i][1]);
+        com[clothes[i][1]]++;
     }
-    sort(kind.begin(), kind.end());
-    auto last = unique(kind.begin(), kind.end());
-    kind.erase(last, kind.end());
 
-
-    return answer;
+    for (pair<string, int> p : com) {
+        answer *= (p.second + 1);
+    }
+    return answer - 1;
 }
