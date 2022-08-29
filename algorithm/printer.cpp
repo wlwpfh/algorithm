85334c2586f8b0a695d71ada1c4c74bb7a2dc55e
yangjiyoung <0112jyoung@naver.com>
2022-08-29 22:19:29 +0900

    프린터 (queue)

--- algorithm/printer.cpp
+++ algorithm/printer.cpp
@@ -5,39 +5,35 @@ using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    int location_index = 0; //위치가 변하니까 
-    int tmp_max = 0;
-    queue<int> q;
-    int tmp_front;
-    // queue<string> index;
-    for (int i = 0; i < priorities.size(); i++)
-    {
-        q.push(priorities[i]);
+    queue<pair<int, int>> q;
+    priority_queue<int> pq;
 
-        if (q.back() > tmp_max)
-            tmp_max = q.back();
+    for (int i = 0; i < priorities.size(); i++) {
+        q.push(make_pair(i, priorities[i]));
+        pq.push(priorities[i]);
     }
+    int i = 0;
+    while (!q.empty()) {
+        // printf("q.front():%d, pq.top():%d, i:%d answer:%d \n",
+           //    q.front(), pq.top(), i, answer);
+        pair<int, int> tmp = q.front();
+        if (tmp.second == pq.top()) {
+            //인쇄하는 중
+            pq.pop();
+            q.pop();
+            answer++;
+            if (tmp.first == location)
+                break;
 
-    printf("tmp_max: %d ", tmp_max);
-    while (1) {
-        if (q.size() == 0) //해당 값이 pop되면
-            break;
-        else {
-            //앞에 있는 게 제일 뽑히고 그게 제일 뒤로 다시 넣기
-
-            // 큐 안에서 늘 제일 큰 수가 뭔 지 확인해야 함
-
-
-
-            if (q.front() == tmp_max)//앞의 값이 제일 큰 수인지
-                q.pop();
-            else {
-                tmp_front = q.front();
-                q.pop();
-                q.push(tmp_front);
-            }
         }
+        else {
+            pair<int, int> current = q.front();
+            q.pop();
+            q.push(current);
+        }
+        i++;
     }
+
 
     return answer;
 }
