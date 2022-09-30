85f41030479d851e78e5e0e00e2770142a3d8b38
yangjiyoung <0112jyoung@naver.com>
2022-09-30 15:32:36 +0900

    크레인 인형뽑기 게임

--- algorithm/crane.cpp
+++ algorithm/crane.cpp
@@ -1,25 +1,28 @@
 #include <string>
 #include <vector>
-#include <queue>
+#include <stack>
 using namespace std;
 
-queue<int> q;
+stack<int> s;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int i, answer = 0;
 
     for (int i = 0; i < moves.size(); i++) {
         int index = moves[i] - 1;
-        int doll = board[index][board[index].size() - 1];
-        board[index].erase(board[index].begin() + board[index].size() - 1); //»Ì±â
-        if (doll == 0)
-            continue;
-        if (!q.empty() && q.front() == doll) {
-            answer += 2;
-            q.pop();
-        }
-        else {
-            q.push(doll);
+        for (int j = 0; j < board[0].size(); j++) {
+            if (board[j][index] == 0)
+                continue;
+
+            if (!s.empty() && s.top() == board[j][index]) {
+                s.pop();
+                answer += 2;
+            }
+            else
+                s.push(board[j][index]);
+
+            board[j][index] = 0;
+            break;
         }
 
     }
