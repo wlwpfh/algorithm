33490cb2414712a3c7314afd3521a35c7aa638be
yangjiyoung <0112jyoung@naver.com>
2022-10-02 21:42:56 +0900

    짝지어 제거하기

---
+++ algorithm/pairremove.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <stack>
+using namespace std;
+
+int solution(string s) {
+    stack<char> st;
+    for (int i = 0; i < s.length(); i++) {
+        if (st.empty() || st.top() != s[i])
+            st.push(s[i]);
+        else if (st.top() == s[i])
+            st.pop();
+    }
+    if (st.empty())
+        return 1;
+    return 0;
+}
