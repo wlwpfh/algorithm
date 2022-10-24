008f64408dc52ba9c0efc1385d1c023c73f5cd81
yangjiyoung <0112jyoung@naver.com>
2022-10-24 23:03:01 +0900

    예상 대진표

--- algorithm/expectable.cpp
+++ algorithm/expectable.cpp
@@ -4,19 +4,12 @@ using namespace std;
 
 int solution(int n, int a, int b)
 {
-    int answer = 1;
+    int answer = 0;
 
-    while (1) {
-        if (a - b == 1 || b - a == 1)
-            break;
-        if (a % 2 == 1)
-            a = a / 2 + 1;
-        else
-            a = a / 2;
-        if (b % 2 == 1)
-            b = b / 2 + 1;
-        else
-            b = b / 2;
+    while (a != b) {
+
+        a = (a + 1) / 2;
+        b = (b + 1) / 2;
         answer++;
     }
 
