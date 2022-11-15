6458fc085ad6b9b1ff177574367679e2c3211214
yangjiyoung <0112jyoung@naver.com>
2022-11-15 23:50:05 +0900

    N개의 최소공배수

--- algorithm/nnumlcm.cpp
+++ algorithm/nnumlcm.cpp
@@ -1,32 +1,20 @@
-#include <string>
 #include <vector>
-#include <map>
 
 using namespace std;
 
-map<int, int> m;
+int gcd(int x, int y) {
+    return x % y == 0 ? y : gcd(y, x % y);
+}
+int lcm(int x, int y) {
+    return (x * y) / gcd(x, y);
+}
 
 int solution(vector<int> arr) {
-    int answer = 0, count = 2;
+    int answer = arr[0];
 
-    for (int i = 0; i < arr.size(); i++) {
-        int tmp = arr[i], count = 2;
-        while (tmp > 1 && count <= tmp) {
-
-            if (tmp % count == 0) {
-                tmp /= count;
-                m[count]++;
-                continue;
-            }
-            count++;
-        }
-
+    for (int i = 1; i < arr.size(); i++) {
+        answer = lcm(answer, arr[i]);
     }
-
-    for (pair<int, int> p : m) {
-        printf("%d -%d°³ \n", p.first, p.second);
-    }
-
 
     return answer;
 }
