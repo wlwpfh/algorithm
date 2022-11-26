6d8dd73c450c3444c807a14cb6a1fa97d4586965
yangjiyoung <0112jyoung@naver.com>
2022-11-26 23:31:23 +0900

    점프와 순간이동

---
+++ algorithm/jumpandteleportation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+
+    while (n != 0) {
+        if (n % 2 == 0)
+            n = n / 2;
+        else {
+            n -= 1;
+            answer++;
+        }
+    }
+    return answer;
+}
