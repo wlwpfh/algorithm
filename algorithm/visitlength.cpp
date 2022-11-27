98e713a6cc7d1c53670bdb2fd8febc931cf2212e
yangjiyoung <0112jyoung@naver.com>
2022-11-27 13:32:54 +0900

    방문 길이

--- algorithm/visitlength.cpp
+++ algorithm/visitlength.cpp
@@ -1,31 +1,33 @@
 #include <string>
-#include <map>
+
 using namespace std;
 
-bool visited[10][10];
-pair<int, int> now = make_pair(0, 0); //현재 위치 저장
+bool visited[11][11][11][11];
+pair<int, int> now = make_pair(0, 0); //현재 위치 저장4
+pair<int, int> adv = make_pair(0, 0);
 int solution(string dirs) {
     int answer = 0;
 
     for (int i = 0; i < dirs.length(); i++) {
+        adv = now;
         if (dirs[i] == 'L')
-            now.first--;
+            adv.first--;
         else if (dirs[i] == 'R')
-            now.first++;
+            adv.first++;
         else if (dirs[i] == 'U')
-            now.second++;
+            adv.second++;
         else
-            now.second--;
+            adv.second--;
 
-        if (now.first == -5 || now.second == -5 || now.first == 5 || now.second == 5)
+        if (adv.first < -5 || adv.first>5 || adv.second < -5 || adv.second>5)
             continue;
 
-        if (visited[now.first + 5][now.second + 5] == 0) {
-            printf("현재 (%d, %d) \n", now.first, now.second);
+        if (!visited[now.first + 5][now.second + 5][adv.first + 5][adv.second + 5]) {
             answer++;
-            visited[now.first + 5][now.second + 5] = 1;
+            visited[now.first + 5][now.second + 5][adv.first + 5][adv.second + 5] = 1;
+            visited[adv.first + 5][adv.second + 5][now.first + 5][now.second + 5] = 1;
         }
-
+        now = adv;
     }
 
     return answer;
