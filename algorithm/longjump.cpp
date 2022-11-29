eb67eaa4df1f8561e40b62bea4c9f31273314f1a
yangjiyoung <0112jyoung@naver.com>
2022-11-29 18:20:05 +0900

    멀리 뛰기

--- algorithm/longjump.cpp
+++ algorithm/longjump.cpp
@@ -1,30 +1,16 @@
-#include <string>
-#include <vector>
-#define DIV 1234567
-using na#include <string>
 #include <vector>
 #define DIV 1234567
 using namespace std;
 vector<int> count;
-void longJump(int n, int curr) {
-    if (n < curr)
-        return;
-    if (n - curr >= 3 && curr > 3) {
-        long long tmp = (count[curr - 2] + count[curr - 1]) % DIV;
-        count.push_back(tmp);
-    }
-    longJump(n, curr + 1);
-}
-
 long long solution(int n) {
-
-    int answer = 0;
 
     count.push_back(1); //1
     count.push_back(2); //2
     count.push_back(3); //3
 
-    longJump(n, 0);
-
-    return count[n];
+    for (int i = 3; i <= n; i++) {
+        long long tmp = (count[i - 2] + count[i - 1]) % DIV;
+        count.push_back(tmp);
+    }
+    return count[n - 1];
 }
