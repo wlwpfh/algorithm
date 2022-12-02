8a539cb388d55a190855cd8ec1baf9a76ba9f5af
yangjiyoung <0112jyoung@naver.com>
2022-12-02 15:10:48 +0900

    등굣길 (dp)

--- algorithm/waytoschool.cpp
+++ algorithm/waytoschool.cpp
@@ -1,5 +1,5 @@
 #include <vector>
-#include<stdio.h>
+
 #define DIV 1000000007
 using namespace std;
 
@@ -8,28 +8,21 @@ int solution(int m, int n, vector<vector<int>> puddles) {
 
     vector<vector<int>> map(n + 1, vector<int>(m + 1, 0));
 
-    for (i = 1; i <= n; i++) {
-        map[i][1] = 1;
-    }
-    for (i = 1; i <= m; i++)
-        map[1][i] = 1;
+    map[1][1] = 1;
+
     for (i = 0; i < puddles.size(); i++) {
-        map[puddles[i][0]][puddles[i][1]] = -1;
+        map[puddles[i][1]][puddles[i][0]] = -1;
     }
-    for (i = 2; i <= n; i++) {
-        for (j = 2; j <= m; j++) {
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= m; j++) {
             if (map[i][j] == -1)
                 continue;
-            if (map[i - 1][j] == -1) {
-                map[i][j] = map[i][j - 1] % DIV;
-                continue;
-            }
-            if (map[i][j - 1] == -1) {
-                map[i][j] = map[i - 1][j] % DIV;
-                continue;
-            }
-            map[i][j] = (map[i - 1][j] + map[i][j - 1]) % DIV;
+            if (map[i - 1][j] != -1)
+                map[i][j] += map[i - 1][j];
+            if (map[i][j - 1] != -1)
+                map[i][j] += map[i][j - 1];
 
+            map[i][j] %= DIV;
         }
     }
 
