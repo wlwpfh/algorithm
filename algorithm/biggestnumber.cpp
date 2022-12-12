4929b19e657373b9df66cbffa3c27239185d7409
yangjiyoung <0112jyoung@naver.com>
2022-12-12 22:23:56 +0900

    가장 큰 수 (정렬)

--- algorithm/biggestnumber.cpp
+++ algorithm/biggestnumber.cpp
@@ -3,31 +3,27 @@
 #include <algorithm>
 using namespace std;
 
+vector<string> sorted;
+
 bool compare(string a, string b) {
-    if (a[0] > b[0])
-        return a[0] > b[0];
-    if (a[0] == b[0]) {
-        return a > b;
-    }
-    return a[0] > b[0];
+    return a + b > b + a;
 }
 
 string solution(vector<int> numbers) {
     string tmp, answer = "";
 
-    int max, i;
-    vector<string> sorted;
     for (auto a : numbers) {
-        sorted.push_back(to_string(a));
+        string tmp = to_string(a);
+        sorted.push_back(tmp);
     }
     sort(sorted.begin(), sorted.end(), compare);
 
-    //제일 앞에 오는 숫자가 큰게 오도록
-    //만약 숫자가 같은게 있다면 그 다음 글자숫자가 없는 애로.. 
-    //그것도 있다면 큰게 오도록
     for (auto a : sorted) {
-        printf("%s \n", a.c_str());
+
         answer += a;
+
+        if (answer[0] == '0')
+            break;
     }
 
     return answer;
