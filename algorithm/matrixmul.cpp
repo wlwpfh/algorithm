c3915440e1ddc29414c05c260ec0bed0a62aacae
yangjiyoung <0112jyoung@naver.com>
2022-12-14 15:32:15 +0900

    행렬의 곱셈

---
+++ algorithm/matrixmul.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer(arr1.size(), vector<int>(arr2[0].size(), 0));
+    for (int i = 0; i < arr1.size(); i++) {
+        for (int j = 0; j < arr1[i].size(); j++) {
+            for (int k = 0; k < arr2[j].size(); k++) {
+                answer[i][k] += arr1[i][j] * arr2[j][k];
+            }
+        }
+    }
+    return answer;
+}
