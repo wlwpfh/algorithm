e4fb3b6c439e9d3b816df53de1657e5ace4fc41a
yangjiyoung <0112jyoung@naver.com>
2022-12-17 21:47:21 +0900

    더 맵게

--- algorithm/morespicy.cpp
+++ algorithm/morespicy.cpp
@@ -1,48 +1,33 @@
-#include <string>
+#include <queue>
 #include <vector>
-#include <algorithm>
-#include <functional>
+
 using namespace std;
 
+priority_queue<int, vector<int>, greater<int> > pq;
+
 int solution(vector<int> scoville, int K) {
-    int answer = 0, tmp = 0, count = 0, i = 0, j;
-    make_heap(scoville.begin(), scoville.end()); //front가 12
-    sort_heap(scoville.begin(), scoville.end()); //front가 1
-    //printf("%d ",scoville.back());
+    int answer = 0;
+
+    for (int i = 0; i < scoville.size(); i++)
+        pq.push(scoville[i]);
+
     while (1) {
-        if (scoville.back() < K) {
-            break;
+        if (pq.top() < K && pq.size() == 1) {
             answer = -1;
-        }
-
-        if (scoville.front() > K) {
-            //heap안의 모든 수가 K보다 높은 경우 break로 빠져나오기
             break;
         }
-        else {
-            tmp = scoville[i] + scoville[i + 1] * 2;
-            //printf("i: %d ,",scoville[i]);
-            //printf("i+1 : %d \n",scoville[i+1]);
-            //printf("%d = %d + %d \n",tmp, scoville[i], scoville[i+1]*2);
-            scoville.erase(scoville.begin());
-            scoville.erase(scoville.begin());
-            scoville.push_back(tmp); answer++;
-            //printf("새로 들어간 값:%d+ (%d*2)= %d \n",scoville[i], scoville[i+1]+1,tmp);
-            i += 2;
+        if (pq.top() >= K)
+            break;
 
-            //sort_heap(scoville.begin(), scoville.end());
+        int first = pq.top();
+        pq.pop();
+        int second = pq.top();
+        pq.pop();
 
- //               for(j=0;j<scoville.size();j++)
- //                   printf("%d ",scoville[j]);
+        int tmp = first + second * 2;
+        pq.push(tmp);
 
-        }
-        make_heap(scoville.begin(), scoville.end());
-        sort_heap(scoville.begin(), scoville.end());
-        // printf("새로 정렬된 heap: ");
-        // for(i=0;i<scoville.size();i++)
-        //     printf("%d ",scoville[i]);
-        // printf("\n");
+        answer++;
     }
-
     return answer;
 }
