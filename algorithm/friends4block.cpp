cc111fd8c61b5d3e503bf4c1527ca5f0c11917d0
yangjiyoung <0112jyoung@naver.com>
2023-03-24 16:26:24 +0900

    프렌즈 4 블록

--- algorithm/friends4block.cpp
+++ algorithm/friends4block.cpp
@@ -17,22 +17,24 @@ void printBoard(vector<string> board) {
             printf("%c ", board[i][j]);
         printf("\n");
     }
+    printf("\n");
     return;
 }
 
 vector<string> moveDown(vector<string> board) {
-    for (int i = 0; i < board.size(); i++) {
-        for (int j = 0; j < board[i].length(); j++) {
-            for (int k = j; k < board[i].length() - 1; k++) {
-                if (removed[i][k + 1]) {
+    int N = board.size();
+    int M = board[0].length();
+    for (int i = 0; i < M; i++) {
+        for (int j = 1; j < N; j++) {
+            for (int k = j; k > 0; k--) {
+                if (board[k][i] == '0') {
                     //밑에가 비어있다면
-                    board[i][k + 1] = board[i][k];
-                    board[i][k] = 0;
+                    board[k][i] = board[k - 1][i];
+                    board[k - 1][i] = '0';
                 }
             }
         }
-    }
-
+    } //맨 아래에서 위로 
     return board;
 }
 
@@ -40,11 +42,12 @@ int solution(int m, int n, vector<string> board) {
     int answer = 0, reduce = 0;
 
     while (1) {
+        initRemoved(m, n);
         reduce = 0;
         //4블록 없애기
         for (int i = 0; i < m - 1; i++) {
             for (int j = 0; j < n - 1; j++) {
-                if ((board[i][j] == board[i + 1][j]) && (board[i][j] == board[i][j + 1]) && (board[i + 1][j] == board[i + 1][j + 1])) {
+                if ((board[i][j] == board[i + 1][j]) && (board[i][j] == board[i][j + 1]) && (board[i + 1][j] == board[i + 1][j + 1]) && board[i][j] != '0') {
                     if (removed[i][j] == false)
                         reduce++;
                     if (removed[i + 1][j] == false)
@@ -57,21 +60,25 @@ int solution(int m, int n, vector<string> board) {
                     removed[i][j + 1] = true;
                     removed[i + 1][j] = true;
                     removed[i + 1][j + 1] = true;
-
-
                 }
                 else
                     continue;
             }
-        }
+        } //줄어든 거 확인하기 
 
         if (reduce == 0) //줄어든 게 없을 때까지
             break;
 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (removed[i][j])
+                    board[i][j] = '0';
+            }
+        }
+
         board = moveDown(board);
-        printBoard(board);
+        ///printBoard(board);
         answer += reduce;
-        //initRemoved(m,n);
     }
     return answer;
 }
